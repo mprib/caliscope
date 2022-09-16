afbddf6bb68bf0bf3a4254ac7ee97147d7d1211b
mprib <prible@gmail.com>
2022-09-16 09:10:53 -0500

    about to simplify by assigning charuco to self

--- src/main.py
+++ src/main.py
@@ -7,12 +7,6 @@ from charuco import Charuco
 charuco = Charuco(4,5,11,8.5)
 
 
-
-
-# charuco.save_image("sample_charuco.png", inverted=True)
-# print(charuco.get_connected_corners())
-# feeds = {0: "Cam_1",1:"Cam_2"}
-
 feeds = {0: "Cam_1"}
 
 vid_file = 'videos\charuco.mkv'
@@ -21,23 +15,14 @@ vid_file = 'videos\charuco.mkv'
 active_camera = Camera(0, "Cam_1")
 active_camera.collect_calibration_corners(
     board_threshold=0.5,
-    charuco = charuco, 
+    self.charuco = charuco, 
     charuco_inverted=True,
     time_between_cal=.5) # seconds that must pass before new corners are stored
 
 
 
 
-
 # %%
 
-
-for stream, stream_name in feeds.items():
-    active_camera = Camera(stream, stream_name)
-    active_camera.collect_calibration_corners(
-        board_threshold=0.5,
-        charuco = charuco, 
-        charuco_inverted=True,
-        time_between_cal=.5) # seconds that must pass before new corners are stored
-
-    active_camera.calibrate()
+active_camera.close()
+# %%

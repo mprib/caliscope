5105e7d68ce9ff272feccdeaeacdc19b7cd0ca04
mprib <prible@gmail.com>
2022-10-02 10:42:25 -0500

    freezing reqs prior to new env

--- src/calibration/concurrency_tutorial/threading.py
+++ src/calibration/concurrency_tutorial/threading.py
@@ -2,23 +2,27 @@ from threading import Thread
 import cv2, time
  
 class VideoStreamWidget(object):
-    def __init__(self, src=0):
+    def __init__(self, src):
+        self.FPS = 30
+
         self.capture = cv2.VideoCapture(src)
+        self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)  # from https://stackoverflow.com/questions/58293187/opencv-real-time-streaming-video-capture-is-slow-how-to-drop-frames-or-get-sync
         # Start the thread to read frames from the video stream
         self.thread = Thread(target=self.update, args=())
         self.thread.daemon = True
         self.thread.start()
+        self.frame_name = "Cam"+str(src)
 
     def update(self):
         # Read the next frame from the stream in a different thread
         while True:
             if self.capture.isOpened():
                 (self.status, self.frame) = self.capture.read()
-            time.sleep(.01)
+            time.sleep(1/self.FPS)
     
     def show_frame(self):
         # Display frames in main program
-        cv2.imshow('frame', self.frame)
+        cv2.imshow(self.frame_name, self.frame)
         key = cv2.waitKey(1)
         if key == ord('q'):
             self.capture.release()
@@ -26,9 +30,16 @@ class VideoStreamWidget(object):
             exit(1)
 
 if __name__ == '__main__':
-    video_stream_widget = VideoStreamWidget()
+
+    src_list = [0,1]
+    cam_widgets = []
+    for src in src_list:
+        cam_widgets.append(VideoStreamWidget(src))
+
     while True:
         try:
-            video_stream_widget.show_frame()
+            for cam in cam_widgets:
+                cam.show_frame()
+            # cam1.show_frame()
         except AttributeError:
             pass

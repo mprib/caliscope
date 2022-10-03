70a85cc0c37aa42f83500c10708701208f2e2c64
mprib <prible@gmail.com>
2022-10-03 12:48:31 -0500

    about to refactor to push frame draw to  main

--- src/calibration/concurrency_tutorial/video_stream_widget.py
+++ src/calibration/concurrency_tutorial/video_stream_widget.py
@@ -1,12 +1,13 @@
 from threading import Thread
 import cv2, time
 import sys
-import detect_2D_points
+import mediapipe as mp
 
+# import detect_2D_points
 
-class VideoStreamWidget(object):
+class VideoStreamWidget:
     def __init__(self, src, width, height):
-        self.FPS_target = 40
+        self.FPS_target = 30
 
         self.capture = cv2.VideoCapture(src)
         self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)  # from https://stackoverflow.com/questions/58293187/opencv-real-time-streaming-video-capture-is-slow-how-to-drop-frames-or-get-sync
@@ -19,17 +20,30 @@ class VideoStreamWidget(object):
         self.frame_name = "Cam"+str(src)
 
         # initialize time trackers of frame updates
-        self.current_time = 0
-        self.previous_time = 0
+        self.start_time = time.time()
+        # self.previous_time = 0
+        self.avg_delta_time = None
+
+        # Mediapipe hand detection infrastructure
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands()
+        self.mpDraw = mp.solutions.drawing_utils 
+        self.connected_landmarks = self.mpHands.HAND_CONNECTIONS
 
     def get_FPS_actual(self):
         """set the actual frame rate from within the update function"""
-        self.current_time = time.time()
-        delta_time = self.current_time-self.previous_time
-        self.previous_time = self.current_time
+        self.delta_time = time.time() - self.start_time
+        self.start_time = time.time()
+        if not self.avg_delta_time:
+            self.avg_delta_time = self.delta_time
+        self.avg_delta_time = 0.8*self.avg_delta_time + 0.2*self.delta_time
+        self.previous_time = self.start_time
 
-        return 1/delta_time
-
+        return 1/self.avg_delta_time
+    
+    def draw_hands(self):
+        imgRGB  = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+        self.hand_results = self.hands.process(imgRGB)
 
 
     def update(self):
@@ -37,17 +51,23 @@ class VideoStreamWidget(object):
         while True:
             if self.capture.isOpened():
                 (self.status, self.frame) = self.capture.read()
-            # wait to read next frame in order to hit target FPS. Record FPS
-            detect_2D_points.get_hands(self.frame)
-            self.FPS_actual = self.get_FPS_actual() 
-            time.sleep(1/self.FPS_target)
+                # wait to read next frame in order to hit target FPS. Record FPS
+                self.draw_hands()
+                self.FPS_actual = self.get_FPS_actual() 
+                time.sleep(1/self.FPS_target)
     
     def show_frame(self):
+
+        # draw hand dots and lines
+        if self.hand_results.multi_hand_landmarks:
+            for handLms in self.hand_results.multi_hand_landmarks:
+                self.mpDraw.draw_landmarks(self.frame, handLms, self.mpHands.HAND_CONNECTIONS)
+
         # Display frames in main program
-        cv2.putText(self.frame, str(int(round(self.FPS_actual, 0))), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,(255,0,255), 3)
+        display_text = "FPS:" + str(int(round(self.FPS_actual, 0)))
+        cv2.putText(self.frame, display_text, (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,(255,0,255), 3)
         cv2.imshow(self.frame_name, self.frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
+        if cv2.waitKey(1) == ord('q'):
             self.capture.release()
             cv2.destroyAllWindows()
             exit(0)

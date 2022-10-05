5c7251f1ca5bfd7ad9bc51ac884e4281cc738907
mprib <prible@gmail.com>
2022-10-05 14:14:48 -0500

    locking code before adding queue

--- src/concurrency_tutorial/video_stream_widget.py
+++ src/concurrency_tutorial/video_stream_widget.py
@@ -23,7 +23,7 @@ class VideoCaptureWidget:
         self.thread.daemon = True
         self.thread.start()
         self.frame_name = "Cam"+str(src)
-
+    
         # initialize time trackers of frame updates
         self.start_time = time.time()
         # self.previous_time = 0
@@ -33,6 +33,7 @@ class VideoCaptureWidget:
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands()
         self.mpDraw = mp.solutions.drawing_utils 
+        self.show_medipipe = True
 
     def get_FPS_actual(self):
         """set the actual frame rate from within the update function"""
@@ -53,44 +54,51 @@ class VideoCaptureWidget:
         # Grap frame and run image detection
         while True:
             if self.capture.isOpened():
-                loop_start = time.time()
                 # pull in working frame
                 (self.status, working_frame) = self.capture.read()
+
                 frame_RGB  = cv2.cvtColor(working_frame, cv2.COLOR_BGR2RGB)
-                self.hand_results = self.hands.process(frame_RGB)
-                            
-                # draw hand dots and lines
+
+                # Only calculate mediapipe if going to display it
+                if self.show_medipipe:
+                    self.hand_results = self.hands.process(frame_RGB)
+
                 self.frame = working_frame.copy() 
-                if self.hand_results.multi_hand_landmarks:
-                    for handLms in self.hand_results.multi_hand_landmarks:
-                        self.mpDraw.draw_landmarks(self.frame, handLms, self.mpHands.HAND_CONNECTIONS)
+                
+                # only draw hand landmarks if calculating mediapipe
+                if self.show_medipipe:
+                    # draw hand dots and lines
+                    if self.hand_results.multi_hand_landmarks:
+                        for handLms in self.hand_results.multi_hand_landmarks:
+                            self.mpDraw.draw_landmarks(self.frame, handLms, self.mpHands.HAND_CONNECTIONS)
+
                 # wait to read next frame in order to hit target FPS. Record FPS
-                loop_stop = time.time()
-
-                dt = loop_stop - loop_start
-                if 1/dt < self.FPS_target:
-                    wait_time = 1/(self.FPS_target-1/dt)
-
                 self.FPS_actual = self.get_FPS_actual() 
-                # time.sleep(wait_time)    #TODO: adjust sleep time to incorporate worker loop time
     
     def grab_frame(self):
         
         self.fps_text =  str(int(round(self.FPS_actual, 0))) 
-        self.time_now = str(datetime.now().strftime("%H:%M:%S"))
+        self.time_now = str(datetime.now().strftime("%S"))
+        self.sec_now = self.time_now[1]
+
+        # if int(self.sec_now) < 5:
+        #     self.show_medipipe = False
+        # else:
+        #     self.show_medipipe = True
+
         cv2.putText(self.frame, "FPS:" + self.fps_text, (10, 70),cv2.FONT_HERSHEY_PLAIN, 2,(0,0,255), 3)
-        # cv2.putText(self.frame, "Time:" + self.time_now, (10,140),cv2.FONT_HERSHEY_PLAIN, 2,(0,0,255), 3)
+        cv2.putText(self.frame, "Time:" + self.sec_now, (10,140),cv2.FONT_HERSHEY_PLAIN, 2,(0,0,255), 3)
         
 
 # Highlight module functionality. View a frame with mediapipe hands
 # press "q" to quit
 if __name__ == '__main__':
-    src_list = [0,1]
-    # src_list = [0]
+    # src_list = [0,1]
+    src_list = [0]
     cam_widgets = []
 
     for src in src_list:
-        cam_widgets.append(VideoCaptureWidget(src, 1080, 640))
+        cam_widgets.append(VideoCaptureWidget(src, 640, 480))
 
     while True:
         try:

c9f0e01edc443d8f7c63f3e38a08f6263d65633e
mprib <prible@gmail.com>
2022-10-06 12:30:11 -0500

    about to encapsulate queue in capture widget

--- src/concurrency_tutorial/video_stream_widget.py
+++ src/concurrency_tutorial/video_stream_widget.py
@@ -17,14 +17,17 @@ class VideoCaptureWidget:
         self.capture = cv2.VideoCapture(src)
         self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)  # from https://stackoverflow.com/questions/58293187/opencv-real-time-streaming-video-capture-is-slow-how-to-drop-frames-or-get-sync
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        # self.mp_toggle_q = mp_toggle_q
-
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+        # Queue used to toggle mp calculation on and off
+        self.mp_toggle_q = mp_toggle_q
+
         # Start the thread to read frames from the video stream
-        self.thread = Thread(target=self.update, args=(mp_toggle_q, ))
+        self.thread = Thread(target=self.update, args=(self.mp_toggle_q, ))
         self.thread.daemon = True
         self.thread.start()
         self.frame_name = "Cam"+str(src)
+        
     
         # initialize time trackers of frame updates
         self.start_time = time.time()
@@ -36,6 +39,10 @@ class VideoCaptureWidget:
         self.hands = self.mpHands.Hands()
         self.mpDraw = mp.solutions.drawing_utils 
         self.show_medipipe = True
+    
+    def get_mediapipe_status(self):
+        return self.show_medipipe
+        
 
     def get_FPS_actual(self):
         """set the actual frame rate from within the update function"""
@@ -49,7 +56,7 @@ class VideoCaptureWidget:
         return 1/self.avg_delta_time
     
 
-    def update(self, mp_toggle_q):
+    def update(self, show_mp_q):
         """
         Worker function that is spun up by Thread
 
@@ -61,8 +68,8 @@ class VideoCaptureWidget:
         while True:
             
             # check to see if another thread signaled a change to mediapipe overley
-            if not mp_toggle_q.empty():
-                self.show_medipipe = mp_toggle_q.get()
+            if not show_mp_q.empty():
+                self.show_medipipe = show_mp_q.get()
 
             if self.capture.isOpened():
                 # pull in working frame
@@ -88,7 +95,6 @@ class VideoCaptureWidget:
     
     def toggle_mediapipe(self):
 
-        print(self.show_medipipe)
         if self.show_medipipe == True:
             self.show_medipipe == False
         else:
@@ -102,13 +108,7 @@ class VideoCaptureWidget:
         self.time_now = str(datetime.now().strftime("%S"))
         self.sec_now = self.time_now[1]
 
-        # if int(self.sec_now) < 5:
-        #     self.show_medipipe = False
-        # else:
-        #     self.show_medipipe = True
-
         cv2.putText(self.frame, "FPS:" + self.fps_text, (10, 70),cv2.FONT_HERSHEY_PLAIN, 2,(0,0,255), 3)
-        cv2.putText(self.frame, "Time:" + self.sec_now, (10,140),cv2.FONT_HERSHEY_PLAIN, 2,(0,0,255), 3)
         
 
 # Highlight module functionality. View a frame with mediapipe hands
@@ -117,10 +117,9 @@ if __name__ == '__main__':
     src_list = [0,1]
     # src_list = [0]
     cam_widgets = []
-    q_s = []
+
     for src in src_list:
         q = queue.Queue()
-        q_s.append(q)   
         cam_widgets.append(VideoCaptureWidget(src, 640, 480, q))
 
     while True:
@@ -131,15 +130,25 @@ if __name__ == '__main__':
                 
         except AttributeError:
             pass
-     
+        
+        # toggle mediapipe with 'm' 
+        if cv2.waitKey(1) == ord('m'):
+            print("Turning Mediapipe On")
+            for cam in cam_widgets:
+                print(cam.frame_name)
+                cam.mp_toggle_q.put(True)
+
+        if cv2.waitKey(1) == ord('n'):
+            print("Turning Mediapipe Off")
+            for cam in cam_widgets:
+                print(cam.frame_name)
+                cam.mp_toggle_q.put(False)
+
+           
+       
+        # 'q' to quit
         if cv2.waitKey(1) == ord('q'):
             for cam in cam_widgets:
-                cam.capture.release()
-            cv2.destroyAllWindows()
-            exit(0)
-        if cv2.waitKey(1) == ord('m'):
-            for cam in cam_widgets:
-
                 cam.capture.release()
             cv2.destroyAllWindows()
             exit(0)

35f1a4ed51f1aadac16d3cde496d6fd4dcb5308b
mprib <prible@gmail.com>
2022-10-06 11:48:04 -0500

    about to start messing with queue implementation

---
+++ src/gui/dynamically_add_widgets.py
@@ -0,0 +1,9 @@
+# https://www.youtube.com/watch?v=4BZL3cF_Dww
+# This module builds off of the tutorial above to add in widgets to a central
+# scroll area. My hope is that this will provide the foundation for the various
+# video display widgets while moving through the camera setup calibration
+# and capture process.
+
+# There should also, I believe, but a window to assess the quality of the 
+# calibration using the Charuco. Is the ultimate reconstruction of the 
+# charuco flat and of the same dimensions as the real one? 

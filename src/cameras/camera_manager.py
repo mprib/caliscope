a4fc6845b146233d735e18a7bcff507229320fca
mprib <prible@gmail.com>
2022-10-09 16:21:25 -0500

    minor cleanup

--- src/cameras/camera_manager.py
+++ src/cameras/camera_manager.py
@@ -15,11 +15,9 @@
 #
 # New camera configurations 
 #%%
+
 import cv2
 from threading import Thread
-
-# from cameras.get_frame_size import get_nearest_resolution
-
 
 TEST_FRAME_COUNT = 3
 MAX_RESOLUTION_CHECK = 10000
@@ -41,7 +39,8 @@ class CameraManager(object):
             self.src = src
             self.capture = test_capture
             self.active_port = True
-            # self._exposure = -5
+            self.stream_active = False
+            self.show_me_active = False
 
             self.set_exposure()
             self.set_default_resolution()
@@ -56,9 +55,6 @@ class CameraManager(object):
     @property
     def exposure(self):
         return self._exposure
-        # print("Getting exposure")
-        # I'm not convinced that the capture device is sending back correct
-        # exposure information, so I will just track independently
     
     @exposure.setter
     def exposure(self, value):
@@ -92,30 +88,47 @@ class CameraManager(object):
 
     @resolution.setter
     def resolution(self, value):
-    
+        if self.show_me_active:
+            self.show_me_active = False
+        
+        if self.stream_active:
+            # self.stop_q.put("Stop")
+            self.capture.release()
+            self.capture = cv2.VideoCapture(self.src)
+        
         self.width = value[0]
         self.height = value[1]
-
+        
 
     def show_me_worker(self, win_name=None): 
         if not win_name:
             win_name = f"'q' to quit video {self.src}"
+        
         while True:
+
             success, frame = self.capture.read()
             cv2.imshow(win_name, frame)
 
             if cv2.waitKey(1) ==ord('q'):
                 cv2.destroyWindow(win_name)
                 break
+                        
 
     def show_me(self, win_name=None):
-        thread = Thread(target=self.show_me_worker, args= (win_name, ), daemon=True)
-        thread.start()
+        """ Note, this is just for general observation purposes, 
+        I need to see how things work in the display widget where
+        it actually matters. This may then neceessitate some other method
+        of handling things."""
+        self.stream_thread = Thread(target=self.show_me_worker, args= (win_name, ), daemon=True)
+        self.stream_thread.start()
 
     def set_default_resolution(self):
+        """called at initilization before anything has changed"""
         self.default_resolution = self.resolution
 
     def set_exposure(self):
+        """Need an initial value, though it does not appear that updates to 
+        exposure reliably read back from OpenCV"""
         self._exposure = self.capture.get(cv2.CAP_PROP_EXPOSURE)
 
     def get_nearest_resolution(self, test_width):
@@ -160,49 +173,3 @@ cam1 = CameraManager(1)
 
 #%% 
 cam1.show_me()
-import time
-for i in range(-7,0):
-    print(i)
-    cam1.capture.set(cv2.CAP_PROP_EXPOSURE, i)
-    print("Actual Exposure is: " + str(cam1.capture.get(cv2.CAP_PROP_EXPOSURE)))
-    time.sleep(2)
-# cam1.show_me()
-print(f"Cam1 Exposure is: {cam1.exposure}")
-
-cam1.exposure = -4
-
-print(f"Cam1 Exposure is: {cam1.exposure}")
-#%%
-cam1.get_resolution()
-
-#%%
-cam1.get_min_resolution()
-#%%
-cam1.get_max_resolution()
-#%%
-cam3 = CameraManager(1)
-#%%
-cam3.get_resolution()
-
-#%%
-cam3.get_min_resolution()
-#%%
-cam3.get_max_resolution()
-#
-#%%
-cams = []
-for i in range(0,4):
-
-    if i != 2:
-        print(f"Configuring Camera {i}")
-        print("Attempting to Create...")
-        cams.append(CameraManager(i))
-    
-
-# %%
-for cam in cams:
-    print(f"Camera at port {cam.src}")
-    print(f"Active Port?: {cam.active_port}")
-    print(f"Exposure: {cam.exposure}")
-    cam.show_me()
-# %%

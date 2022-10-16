6445da4951be24597e8e798f62cb95225b032e0e
mprib <prible@gmail.com>
2022-10-15 19:15:07 -0500

    create corner detector for RTD

---
+++ src/calibration/corner_detector.py
@@ -0,0 +1,17 @@
+
+
+
+from charuco import Charuco
+
+class CornerDetector:
+
+    def __init__(self, charuco):
+        self.charuco = charuco
+
+
+
+
+if __name__ == "__main__":
+
+    charuco = Charuco(4,5,11,8.5,aruco_scale = .75, square_size_overide=.0525)
+

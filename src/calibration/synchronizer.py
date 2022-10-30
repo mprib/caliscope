3f914648467e386633bd217e7e667836b71990bf
mprib <prible@gmail.com>
2022-10-30 14:12:17 -0500

    earliest next frame error handling working..

--- src/calibration/synchronizer.py
+++ src/calibration/synchronizer.py
@@ -3,10 +3,10 @@
 # index. If one frame was read after the earliest frame in the next index,
 # move it to the next index
 
-
-
 import json
 import logging
+import time
+from tkinter import E
 
 logging.basicConfig(filename="synchronizer.log", filemode = "w", level=logging.DEBUG)
 
@@ -14,43 +14,54 @@ logging.basicConfig(filename="synchronizer.log", filemode = "w", level=logging.D
 with open("frame_data.json",) as f:
     frame_data = json.load(f)
 
-
-
-# blank_layer = [None for _ in range(len(ports))]
-
-# lay down initial layer of frames
-# first_layer = blank_layer
-
 # get minimum value of frame_time for next layer
-def earliest_next_frame(ports, current_port_frame_indices, frame_data):
-    next_frame_times = []
-    for port in ports:
-        next_index = current_port_frame_indices[port] + 1
-        # port_index_key = str(port) + "_" + str(next_index)
-        next_frame_at = frame_data[str(port)][next_index]["frame_time"]
-        next_frame_times.append(next_frame_at)
+def earliest_next_frame(wait_retry = 0.03, attempt=0):
+    
+    try:
+        time_of_next_frames = []
+        for port in ports:
+            next_index = port_current_frame[port] + 1
+            # port_index_key = str(port) + "_" + str(next_index)
+            next_frame_time = frame_data[str(port)][next_index]["frame_time"]
+            time_of_next_frames.append(next_frame_time)
+        return min(time_of_next_frames)
 
-    return min(next_frame_times)
+    except IndexError:
+        if attempt > 5:
+            raise IndexError
+        logging.error("Not enough new frames available. Waiting for more frames...")
+        time.sleep(wait_retry)
+        logging.debug("Reattempting to get earliest next frame")
+        earliest_next_frame(attempt=attempt+1)
+
 
 ports = [0,1,2]
 synched_frames = []
-current_port_frame_indices = [0 for _ in range(len(ports))]
+port_current_frame = [0 for _ in range(len(ports))]
 
-for _ in range(100):
+while True:
 
-    cutoff_time = earliest_next_frame(ports, current_port_frame_indices, frame_data)
+    try:
+        cutoff_time = earliest_next_frame()
+    except IndexError as e:
+        print(e)
+        break
+
     next_layer = []
 
     for port in ports:
-        port_frame_index = current_port_frame_indices[port]
+        current_frame = port_current_frame[port]
         # frame_data = all_frames[str(port) + "_" + str(port_frame_index)]
-        frame_time = frame_data[str(port)][port_frame_index]["frame_time"]
-        placeholder = f"{port}_{port_frame_index}_{frame_time}"
+        current_frame_data = frame_data[str(port)][current_frame]
+        frame_time = current_frame_data["frame_time"]
 
-        if frame_data[str(port)][port_frame_index]["frame_time"] < cutoff_time:
+        # placeholder here is where the actual corner data would go
+        placeholder = f"{port}_{current_frame}_{frame_time}"
+
+        if frame_time < cutoff_time:
             #add the data and increment the index
             next_layer.append(placeholder)      
-            current_port_frame_indices[port] +=1
+            port_current_frame[port] +=1
         else:
             next_layer.append(None)
 

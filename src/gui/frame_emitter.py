930fcc0ac09d29cbe404658103d1b1c711b7deb8
mprib <prible@gmail.com>
2022-11-24 09:11:12 -0600

    sub frame_q as dependency rather than video_stream

--- src/gui/frame_emitter.py
+++ src/gui/frame_emitter.py
@@ -1,69 +1,62 @@
 import logging
-logging.basicConfig(filename="synchronizer.log", 
-                    filemode = "w", 
-                    level=logging.INFO)
+
+logging.basicConfig(filename="synchronizer.log", filemode="w", level=logging.INFO)
 
 import sys
-from pathlib import Path
 import time
+from pathlib import Path
 
 import cv2
-
-from PyQt6.QtCore import Qt, QThread, pyqtSignal, QSize
-from PyQt6.QtGui import QIcon, QImage, QPixmap, QFont
-
-# Append main repo to top of path to allow import of backend
-sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+from PyQt6.QtCore import QSize, Qt, QThread, pyqtSignal
+from PyQt6.QtGui import QFont, QIcon, QImage, QPixmap
 
 
 class FrameEmitter(QThread):
-    # establish signals from the frame that will be displayed in real time 
+    # establish signals from the frame that will be displayed in real time
     # within the GUI
     ImageBroadcast = pyqtSignal(QPixmap)
     FPSBroadcast = pyqtSignal(int)
 
-    
-    def __init__(self, video_stream, pixmap_edge_length=None):
+    def __init__(self, frame_q, pixmap_edge_length=None):
         # pixmap_edge length is from the display window. Keep the display area
         # square to keep life simple.
-        super(FrameEmitter,self).__init__()
-        self.min_sleep = .01 # if true fps drops to zero, don't blow up
-        self.stream = video_stream
+        super(FrameEmitter, self).__init__()
+        self.frame_q = frame_q
         self.pixmap_edge_length = pixmap_edge_length
         print("Initializing Frame Emitter")
-    
+
     def run(self):
-        MIN_SLEEP_TIME = .01
         self.ThreadActive = True
 
-         
         while self.ThreadActive:
-            try:    # takes a moment for capture widget to spin up...don't error out
+            try:  # takes a moment for capture widget to spin up...don't error out
 
                 # Grab a frame from the capture widget and broadcast to displays
-                frame = self.stream.frame
+                frame = self.frame_q.get()
                 image = self.cv2_to_qlabel(frame)
                 pixmap = QPixmap.fromImage(image)
 
-                # GUI was crashing I believe due to overloading GUI thread with 
+                # GUI was crashing I believe due to overloading GUI thread with
                 # scaling. Scaling within the emitter resolved the crashes
 
                 if self.pixmap_edge_length:
-                    pixmap = pixmap.scaled(self.pixmap_edge_length,
-                                           self.pixmap_edge_length,
-                                           Qt.AspectRatioMode.KeepAspectRatio)
+                    pixmap = pixmap.scaled(
+                        self.pixmap_edge_length,
+                        self.pixmap_edge_length,
+                        Qt.AspectRatioMode.KeepAspectRatio,
+                    )
 
                 self.ImageBroadcast.emit(pixmap)
 
                 # grab and broadcast fps
-                fps = self.stream.FPS_actual
+                fps = 0  # TODO: #14 calculate based off of rate of getting from q
                 self.FPSBroadcast.emit(fps)
 
                 # throttle rate of broadcast to reduce system overhead
-                if fps == 0:    # Camera likely reconnecting
-                    time.sleep(MIN_SLEEP_TIME) 
+                if fps == 0:  # Camera likely reconnecting
+                    time.sleep(MIN_SLEEP_TIME)
                 else:
-                    time.sleep(1/fps)
+                    time.sleep(1 / fps)
 
             except AttributeError:
                 pass
@@ -76,15 +69,16 @@ class FrameEmitter(QThread):
         Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         FlippedImage = cv2.flip(Image, 1)
 
-        qt_frame = QImage(FlippedImage.data, 
-                          FlippedImage.shape[1], 
-                          FlippedImage.shape[0], 
-                          QImage.Format.Format_RGB888)
+        qt_frame = QImage(
+            FlippedImage.data,
+            FlippedImage.shape[1],
+            FlippedImage.shape[0],
+            QImage.Format.Format_RGB888,
+        )
         return qt_frame
-
 
 
 if __name__ == "__main__":
     pass
 
-    # not much to look at here... go to camera_config_widget.py for test
+    # not much to look at here... go to camera_config_dialogue.py for test

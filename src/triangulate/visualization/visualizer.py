aa389c16cb3be6e0fa8c0863c95a0ff1a3516721
mprib <prible@gmail.com>
2022-12-13 06:41:40 -0600

    make cm explicit in square size override

--- src/triangulate/visualization/visualizer.py
+++ src/triangulate/visualization/visualizer.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     recorded_stream_pool.play_videos()
     # create a corner tracker to locate board corners
     charuco = Charuco(
-        4, 5, 11, 8.5, aruco_scale=0.75, square_size_overide=0.0525, inverted=True
+        4, 5, 11, 8.5, aruco_scale=0.75, square_size_overide_cm=5.25, inverted=True
     )
     trackr = CornerTracker(charuco)
     # create a commmon point finder to grab charuco corners shared between the pair of ports

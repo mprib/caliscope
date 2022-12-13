0469d9932d3894e3332c226af2537ca17debf20f
mprib <prible@gmail.com>
2022-12-13 10:42:00 -0600

    rename examples folder to sessions

--- src/session.py
+++ src/session.py
@@ -257,7 +257,7 @@ class Session:
 #%%
 if __name__ == "__main__":
     repo = Path(__file__).parent.parent
-    config_path = Path(repo, "examples", "default_session")
+    config_path = Path(repo, "sessions", "default_session")
     print(config_path)
     session = Session(config_path)
     session.update_config()

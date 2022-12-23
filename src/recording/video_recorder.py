85cc7934ec87de22d0023c148b89f0b8b5d95c24
mprib <prible@gmail.com>
2022-12-23 13:15:45 -0600

    expand sleep time

--- src/recording/video_recorder.py
+++ src/recording/video_recorder.py
@@ -138,5 +138,5 @@ if __name__ == "__main__":
     print(repo)
     # video_path = Path(repo,"sessions", "recordings", "sample1")
     video_recorder.start_recording(session_path)
-    time.sleep(10)
+    time.sleep(15)
     video_recorder.stop_recording()

00ffdcdb08774e2fc525127618465b1c456febf5
mprib <prible@gmail.com>
2022-12-26 10:12:54 -0600

    convert initial array constructor to function

--- src/calibration/array_constructor.py
+++ src/calibration/array_constructor.py
@@ -9,111 +9,90 @@ import sys
 from pathlib import Path
 import numpy as np
 
-repo = str(Path(__file__)).split("src")[0]
-sys.path.insert(0, repo)
+def get_initial_array(calibration_data):
+    config = toml.load(Path(calibration_data, "config.toml"))
 
-from src.triangulate.stereo_triangulator import StereoTriangulator
+    daisy_chain = {
+        "Pair": [],
+        "Primary": [],
+        "Secondary": [],
+        "error": [],
+        "Rotation": [],
+        "Translation": [],
+    }
 
-class ArrayConstructor():
+    for key, params in config.items():
+        if key.split("_")[0] == "stereo":
+            port_A = key.split("_")[1]
+            port_B = key.split("_")[2]
 
-    def __init__(self):
-        self.session = session        
+            pair = (port_A, port_B)
+            rotation = np.array(params["rotation"], dtype=np.float64)
+            translation = np.array(params["translation"], dtype=np.float64)
+            error = float(params["RMSE"])
 
-        
-        
+            daisy_chain["Pair"].append(pair)
+            daisy_chain["Primary"].append(port_A)
+            daisy_chain["Secondary"].append(port_B)
 
-if __name__ == "main":
-    from src.session import Session 
+            daisy_chain["Rotation"].append(rotation)
+            daisy_chain["Translation"].append(translation)
+            daisy_chain["error"].append(error)
 
-    session_path = Path(repo, "sessions", "iterative_adjustment")
+    daisy_chain = pd.DataFrame(daisy_chain).sort_values("error")
 
-    session = Session(session_path)
-    config = toml.load(Path(session_path, "config.toml"))
+    # create an inverted version of these to determine best Anchor camera
+    inverted_chain = daisy_chain.copy()
+    inverted_chain.Primary, inverted_chain.Secondary = inverted_chain.Secondary, inverted_chain.Primary
+    inverted_chain.Translation = inverted_chain.Translation * -1
+    inverted_chain.Rotation = inverted_chain.Rotation.apply(np.linalg.inv)
 
-daisy_chain = {
-    "Pair": [],
-    "Config_Key": [],
-    "Primary": [],
-    "Secondary": [],
-    "error": [],
-    "Rotation": [],
-    "Translation": [],
-}
+    daisy_chain_w_inverted = pd.concat([daisy_chain,inverted_chain], axis=0)
+
+    #%%
+    all_pairs = daisy_chain["Pair"].unique()
+    print(all_pairs)
+    #%%
+    # create the inverted formats for all of the rows
+
+    mean_error = (
+        daisy_chain_w_inverted.filter(["Primary", "error"])
+        .groupby("Primary")
+        .agg("mean")
+        .rename(columns={"error": "MeanError"})
+        .sort_values("MeanError")
+    )
+
+    anchor_camera = mean_error.index[0]
+    # %%
+    daisy_chain_w_inverted = daisy_chain_w_inverted.merge(mean_error, how="left", on="Primary").sort_values(
+        "MeanError"
+    )
+
+    # %%
+    daisy_chain_w_inverted.insert(4, 'MeanError', daisy_chain_w_inverted.pop('MeanError'))
+    daisy_chain_w_inverted.sort_values(["MeanError"])
+
+    # %%
+    # need to build an array of cameras in a common frame of reference a starting point for the calibration
+    # if one of the stereo pairs did not get calibrated, then some additional tricks will need to get 
+    # deployed to make things work. But fortunately this is the simpler case now.
+    initial_array = daisy_chain_w_inverted[daisy_chain_w_inverted.Primary==anchor_camera]
+    initial_array = initial_array[["Primary", "Secondary", "Rotation", "Translation"]]
+    # %%
+
+    return initial_array
 
 
-for key, params in config.items():
-    if key.split("_")[0] == "stereo":
-        port_A = key.split("_")[1]
-        port_B = key.split("_")[2]
-
-        # new_key = f"pair_{port_A}_{port_B}"
-        pair = (port_A, port_B)
-        rotation = np.array(params["rotation"], dtype=np.float64)
-        translation = np.array(params["translation"], dtype=np.float64)
-        error = float(params["RMSE"])
-
-
-        daisy_chain["Pair"].append(pair)
-        daisy_chain["Config_Key"].append(key)
-        daisy_chain["Primary"].append(port_A)
-        daisy_chain["Secondary"].append(port_B)
-
-        daisy_chain["Rotation"].append(rotation)
-        daisy_chain["Translation"].append(translation)
-        daisy_chain["error"].append(error)
-
-daisy_chain = pd.DataFrame(daisy_chain).sort_values("error")
-
-#%%
-all_pairs = daisy_chain["Pair"].unique()
-print(all_pairs)
-#%%
-# create the inverted formats for all of the rows
-
-inverted_relationships = {
-    "Pair": [],
-    "Config_Key": [],
-    "Primary": [],
-    "Secondary": [],
-    "error": [],
-    "Rotation": [],
-    "Translation": [],
-}
-rows = daisy_chain.shape[0]
-for index in range(rows):
-    orig_row = daisy_chain.loc[index]
-    inverted_relationships["Pair"].append(orig_row["Pair"])
-    inverted_relationships["Config_Key"].append(orig_row["Config_Key"])
-    inverted_relationships["Primary"].append(orig_row["Secondary"])
-    inverted_relationships["Secondary"].append(orig_row["Primary"])
-    inverted_relationships["error"].append(orig_row["error"])
-    inverted_relationships["Rotation"].append(np.linalg.inv(orig_row["Rotation"]))
-    inverted_relationships["Translation"].append(orig_row["Translation"] * -1)
-
-inverted_relationships = pd.DataFrame(inverted_relationships)
-daisy_chain = pd.concat([daisy_chain, inverted_relationships])
-daisy_chain = daisy_chain.sort_values(["Primary", "Secondary"])
 
 #%%
-# get primary with the lowest RMSE connecting it to daisy_chain.
+if __name__=="__main__":
+    print("Inside main")
+    repo = str(Path(__file__)).split("src")[0]
 
-mean_error = (
-    daisy_chain.filter(["Primary", "error"])
-    .groupby("Primary")
-    .agg("mean")
-    .rename(columns={"error": "MeanError"})
-    .sort_values("MeanError")
-)
-
-print(mean_error)
-
-Lowest_Error = mean_error[mean_error.MeanError == mean_error.MeanError.min()]
-
-print(Lowest_Error)
-# %%
-sorted_chain = daisy_chain.merge(mean_error, how="left", on="Primary").sort_values(
-    "MeanError"
-)
-
-sorted_chain    
+    print("Inside main")
+    sys.path.insert(0, repo)
+    calibration_data = Path(repo, "sessions", "iterative_adjustment")
+    init_array = get_initial_array(calibration_data)
+    print(init_array)
 # %%

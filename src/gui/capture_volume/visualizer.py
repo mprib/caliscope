7a41d8be1ac12d93ac0a9f8b47075ee7d954e457
mprib <prible@gmail.com>
2023-01-02 09:11:19 -0600

    testing with new triangulator framework

--- src/gui/capture_volume/visualizer.py
+++ src/gui/capture_volume/visualizer.py
@@ -12,15 +12,16 @@ import sys
 import time
 from pathlib import Path
 from threading import Thread
+from queue import Queue
 
 import numpy as np
 import pyqtgraph.opengl as gl
 
 from src.triangulate.stereo_triangulator import CameraData
-from src.triangulate.visualization.camera_mesh import CameraMesh
+from src.gui.capture_volume.camera_mesh import CameraMesh
 
 
-class CaptureVolumeVisualizer():
+class CaptureVolumeVisualizer:
     def __init__(self, triangulator):
         self.triangulator = triangulator
         self.point_in_q = None
@@ -40,15 +41,14 @@ class CaptureVolumeVisualizer():
         self.scene.addItem(self.mesh_A)
         self.scene.addItem(self.mesh_B)
 
-
-    def add_point_q(self,q):
+    def add_point_q(self, q):
         self.point_in_q = q
-        
-        self.board_data = self.point_in_q.get()   
+
+        self.board_data = self.point_in_q.get()
 
         self.color = (1, 0, 0, 1)
         self.board_viz = gl.GLScatterPlotItem(
-            pos=self.board_data , color=self.color, size=.01, pxMode=False
+            pos=self.board_data, color=self.color, size=0.01, pxMode=False
         )
 
         self.scene.addItem(self.board_viz)
@@ -58,14 +58,16 @@ class CaptureVolumeVisualizer():
         self.board_viz.setData(pos=self.board_data, color=self.color)
 
     def begin(self):
-        
         def timer_wrkr():
             while True:
-                time.sleep(1/30)
-                self.next_frame() 
-        self.timer_thread = Thread(target=timer_wrkr, args=[],daemon=True)
+                time.sleep(1 / 30)
+                self.next_frame()
+
+        self.timer_thread = Thread(target=timer_wrkr, args=[], daemon=True)
         self.timer_thread.start()
-# 
+
+
+#
 
 # helper functions to assist with scene creation
 def mesh_from_camera(cd: CameraData):
@@ -122,16 +124,19 @@ if __name__ == "__main__":
     from src.recording.recorded_stream import RecordedStreamPool
     from src.triangulate.paired_point_stream import PairedPointStream
     from src.triangulate.stereo_triangulator import StereoTriangulator
-    from src.triangulate.visualization.visualizer import \
-        CaptureVolumeVisualizer
+    from src.gui.capture_volume.visualizer import CaptureVolumeVisualizer
+    from src.cameras.camera_array import CameraArray, CameraArrayBuilder
 
     # set the location for the sample data used for testing
     repo = str(Path(__file__)).split("src")[0]
 
-    session_directory =Path(repo, "sessions", "high_res_session")
-    video_directory = Path(session_directory,"recording")
+    # session_directory = Path(repo, "sessions", "high_res_session")
+    calibration_data = Path(repo, "sessions", "iterative_adjustment")
+    video_directory = Path(calibration_data, "recording")
     # create playback streams to provide to synchronizer
     ports = [0, 2]
+
+    camera_array = CameraArrayBuilder(calibration_data).get_camera_array()
 
     recorded_stream_pool = RecordedStreamPool(ports, video_directory)
     syncr = Synchronizer(recorded_stream_pool.streams, fps_target=None)
@@ -150,13 +155,33 @@ if __name__ == "__main__":
         tracker=trackr,
     )
 
-    config_path = str(Path(session_directory, "config.toml"))
-    triangulatr = StereoTriangulator(point_stream, config_path)
+    camA, camB = camera_array.cameras[0], camera_array.cameras[2]
+    pair = (camA.port, camB.port)
+
+    config_path = str(Path(calibration_data, "config.toml"))
+    test_pair_in_q = Queue(-1)
+
+    triangulatr = StereoTriangulator(camA, camB, test_pair_in_q)
+  
+  
+    # create a thread that will feed the test_pair_in_q with new datapoints 
+    def coordinate_feeder_worker():
+        while True:
+            point_packet = point_stream.out_q.get()
+            if point_packet.pair == pairs[0]:
+                test_pair_in_q.put(point_packet)
+  
+    thread = Thread(target=coordinate_feeder_worker,args=[],daemon=True)
+    thread.start()
+    
+    # TODO TOMORROW: Mac, start here. There is a problem with the visualizer
+    # not receiving points from the triangulator out_q
+    # brain getting fuzzy, but you can figure it out....
 
     app = QApplication(sys.argv)
     vizr = CaptureVolumeVisualizer(triangulatr)
     vizr.add_point_q(triangulatr.out_q)
     vizr.scene.show()
     vizr.begin()
-    
+
     sys.exit(app.exec())

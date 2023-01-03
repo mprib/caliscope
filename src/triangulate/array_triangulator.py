150c335694823a7e0ceb965d8454c041150a3dd9
mprib <prible@gmail.com>
2023-01-02 19:07:07 -0600

    saving output of array triangulation

--- src/triangulate/array_triangulator.py
+++ src/triangulate/array_triangulator.py
@@ -3,11 +3,15 @@ This class will serve as the primary coordinator of the various camera pairs.
 This will only deal with the CameraData object and not the Camera itself, which 
 includes the additional functionality of overhead related to managing
 the videocapture device.
+
+At the moment the priority is getting triangulated data saved out in a csv that could
+then be played back into a visualizer, or used as the basis for an array config optimization.
+
 """
 import logging
 
 LOG_FILE = r"log\array_triangulator.log"
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
 # LOG_LEVEL = logging.INFO
 
 LOG_FORMAT = " %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s"
@@ -16,6 +20,7 @@ logging.basicConfig(filename=LOG_FILE, filemode="w", format=LOG_FORMAT, level=LO
 from itertools import combinations
 from threading import Thread, Event
 from queue import Queue
+import pandas as pd
 
 from src.cameras.camera_array import CameraArray, CameraArrayBuilder
 from src.cameras.synchronizer import Synchronizer
@@ -23,46 +28,79 @@ from src.recording.recorded_stream import RecordedStreamPool
 from src.triangulate.stereo_triangulator import StereoTriangulator
 from src.triangulate.paired_point_stream import PairedPointStream
 
+
 class ArrayTriangulator:
-    
-    def __init__(self, camera_array:CameraArray, paired_point_stream: PairedPointStream ):
+    def __init__(
+        self, camera_array: CameraArray, paired_point_stream: PairedPointStream, save_directory=None
+    ):
         self.camera_array = camera_array
         self.paired_point_stream = paired_point_stream
-        
+        self.save_directory = save_directory
+
         self.ports = [key for key in self.camera_array.cameras.keys()]
-        
-        # initialize stereo triangulators for each pair        
+
+        # initialize stereo triangulators for each pair
         self.stereo_triangulators = {}
-        self.paired_point_qs = {}   # means to pass stereotriangulators new paired points to process
+        self.paired_point_qs = (
+            {}
+        )  # means to pass stereotriangulators new paired points to process
         for pair in self.paired_point_stream.pairs:
             logging.info(f"Creating StereoTriangulator for camera pair {pair}")
             portA = pair[0]
             portB = pair[1]
             camA = self.camera_array.cameras[portA]
             camB = self.camera_array.cameras[portB]
-            pair_q = Queue(-1)
-            self.paired_point_qs[pair] = pair_q
-            self.stereo_triangulators[pair] = StereoTriangulator(camA, camB, pair_q)
+            # pair_q = Queue(-1)
+            self.paired_point_qs[pair] = Queue(-1)
+            self.stereo_triangulators[pair] = StereoTriangulator(camA, camB, self.paired_point_qs[pair])
 
-        self.stop = Event() 
+        self.stop = Event()
 
-        self.thread = Thread(target=self.triangulate_points_worker, args=[], daemon=False)
+        self.thread = Thread(
+            target=self.triangulate_points_worker, args=[], daemon=False
+        )
         self.thread.start()
-        
+
     def triangulate_points_worker(self):
-        
+        aggregate_3d_points = {
+            "pair": [],
+            "time": [],
+            "bundle": [],
+            "id": [],
+            "x_pos": [],
+            "y_pos": [],
+            "z_pos": [],
+        }
+
+
         while not self.stop.is_set():
             new_paired_point_packet = self.paired_point_stream.out_q.get()
-            self.paired_point_qs[new_paired_point_packet.pair].put(new_paired_point_packet)
-            
+            self.paired_point_qs[new_paired_point_packet.pair].put(
+                new_paired_point_packet
+            )
+
             for pair, triangulator in self.stereo_triangulators.items():
                 if not triangulator.out_q.empty():
                     triangulated_packet = triangulator.out_q.get()
-                    print(pair)
-                    print(triangulated_packet.xyz)
+                    packet_data = triangulated_packet.to_dict()
+
+                    for key, value in packet_data.items():
+                        aggregate_3d_points[key].extend(value)
+
+                    print(triangulated_packet.bundle_index)
+                    #TODO: figure out how to get this to stop automatically
+                    # might want to get the frame counts for the saved port data
+                    if triangulated_packet.bundle_index > 270:
+                        self.stop.set()
+
+        
+        if self.save_directory is not None:
+            logging.info(f"Saving triangulated point csv to {self.save_directory}")
+            aggregate_3d_points = pd.DataFrame(aggregate_3d_points)
+            aggregate_3d_points.to_csv(Path(self.save_directory, "triangulated_points.csv"))
 
 if __name__ == "__main__":
-    from pathlib import Path 
+    from pathlib import Path
     from src.calibration.charuco import Charuco
     from src.calibration.corner_tracker import CornerTracker
 
@@ -70,15 +108,16 @@ if __name__ == "__main__":
     repo = str(Path(__file__)).split("src")[0]
     session_directory = Path(repo, "sessions", "iterative_adjustment")
     array_builder = CameraArrayBuilder(session_directory)
-    camera_array = array_builder.get_camera_array()  
+    camera_array = array_builder.get_camera_array()
 
-    
     # Build streams from pre-recorded video
-    ports = [0,1,2]
+    ports = [0, 1, 2]
     recorded_stream_pool = RecordedStreamPool(ports, session_directory)
-    
+
     recorded_stream_pool.play_videos()
-    syncr = Synchronizer(recorded_stream_pool.streams, fps_target=None) # no fps target b/c not playing back for visual display
+    syncr = Synchronizer(
+        recorded_stream_pool.streams, fps_target=None
+    )  # no fps target b/c not playing back for visual display
 
     # create a corner tracker to locate board corners
     charuco = Charuco(
@@ -87,12 +126,12 @@ if __name__ == "__main__":
     trackr = CornerTracker(charuco)
 
     # create a commmon point finder to grab charuco corners shared between the pair of ports
-    pairs = [(0, 1), (0,2), (1,2)]
+    pairs = [(0, 1), (0, 2), (1, 2)]
     point_stream = PairedPointStream(
         synchronizer=syncr,
         pairs=pairs,
         tracker=trackr,
     )
 
-    # Build triangulator    
-    array_triangulator = ArrayTriangulator(camera_array, point_stream)
+    # Build triangulator
+    array_triangulator = ArrayTriangulator(camera_array, point_stream, session_directory)

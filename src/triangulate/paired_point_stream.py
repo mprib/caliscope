f0f4441f0229937b323e465e5a0ae6ffae15f11c
mprib <prible@gmail.com>
2023-01-05 12:00:14 -0600

    save output to csv

--- src/triangulate/paired_point_stream.py
+++ src/triangulate/paired_point_stream.py
@@ -21,7 +21,7 @@ from src.calibration.corner_tracker import CornerTracker
 
 
 class PairedPointStream:
-    def __init__(self, synchronizer, pairs, tracker):
+    def __init__(self, synchronizer, pairs, tracker, csv_output_path=None):
 
         self.bundle_in_q = Queue(-1)
         self.synchronizer = synchronizer
@@ -32,8 +32,23 @@ class PairedPointStream:
         self.out_q = Queue(-1)  # no size limitations...should be small data
         self.pairs = pairs
 
+        self.csv_output_path = csv_output_path
+        self.tidy_output = {}  # a holding place for data to be saved to csv
+
         self.thread = Thread(target=self.find_paired_points, args=[], daemon=True)
         self.thread.start()
+
+    def add_to_tidy_output(self, packet):
+        if self.csv_output_path is None:
+            return
+
+        tidy_packet = packet.to_dict()
+        if len(self.tidy_output) == 0:
+            for key, value in tidy_packet.copy().items():
+                self.tidy_output[key] = value
+        else:
+            for key, value in tidy_packet.copy().items():
+                self.tidy_output[key].extend(value)
 
     def find_paired_points(self):
 
@@ -66,11 +81,11 @@ class PairedPointStream:
                         )
                         logging.debug(f"Port: {port}: \n {points[port]}")
 
-            common_points = None
+            paired = None
             for pair in self.pairs:
                 if pair[0] in points.keys() and pair[1] in points.keys():
                     # print("Entering inner join loop")
-                    common_points = points[pair[0]].merge(
+                    paired = points[pair[0]].merge(
                         points[pair[1]],
                         on="ids",
                         how="inner",
@@ -84,28 +99,16 @@ class PairedPointStream:
 
                     bundle_index = bundle[port_A]["bundle_index"]
 
-                    point_id = np.array(common_points["ids"], dtype=np.int64)
+                    point_id = np.array(paired["ids"], dtype=np.int64)
 
-                    loc_img_x_A = np.array(
-                        common_points["loc_img_x_A"], dtype=np.float64
-                    )
-                    loc_img_x_B = np.array(
-                        common_points["loc_img_x_B"], dtype=np.float64
-                    )
+                    loc_img_x_A = np.array(paired["loc_img_x_A"], dtype=np.float64)
+                    loc_img_x_B = np.array(paired["loc_img_x_B"], dtype=np.float64)
 
-                    loc_img_y_A = np.array(
-                        common_points["loc_img_y_A"], dtype=np.float64
-                    )
-                    loc_img_y_B = np.array(
-                        common_points["loc_img_y_B"], dtype=np.float64
-                    )
+                    loc_img_y_A = np.array(paired["loc_img_y_A"], dtype=np.float64)
+                    loc_img_y_B = np.array(paired["loc_img_y_B"], dtype=np.float64)
 
-                    loc_board_x = np.array(
-                        common_points["loc_board_x_A"], dtype=np.float64
-                    )
-                    loc_board_y = np.array(
-                        common_points["loc_board_y_A"], dtype=np.float64
-                    )
+                    loc_board_x = np.array(paired["loc_board_x_A"], dtype=np.float64)
+                    loc_board_y = np.array(paired["loc_board_y_A"], dtype=np.float64)
 
                     packet = PairedPointsPacket(
                         bundle_index=bundle_index,
@@ -121,12 +124,14 @@ class PairedPointStream:
                         loc_img_x_B=loc_img_x_B,
                         loc_img_y_B=loc_img_y_B,
                     )
-                    logging.debug(
-                        f"Points in common for ports {pair}: \n {common_points}"
-                    )
+                    logging.debug(f"Points in common for ports {pair}: \n {paired}")
 
-                if common_points is not None:
+                if paired is not None:
                     self.out_q.put(packet)
+                    self.add_to_tidy_output(packet)
+                    
+                if bundle_index == 100:
+                    pd.DataFrame(self.tidy_output).to_csv(self.csv_output_path)
 
 
 @dataclass
@@ -154,6 +159,28 @@ class PairedPointsPacket:
     def pair(self):
         return (self.port_A, self.port_B)
 
+    def to_dict(self):
+        """A method that can be used by the caller of the point stream to create a
+        dictionary of lists that is well-formatted to be turned into a tidy dataframe
+        for export to csv."""
+
+        packet_dict = {}
+        length = len(self.point_id)
+        packet_dict["bundle_index"] = [self.bundle_index] * length
+        packet_dict["port_A"] = [self.port_A] * length
+        packet_dict["port_B"] = [self.port_B] * length
+        packet_dict["time_A"] = [self.time_A] * length
+        packet_dict["time_B"] = [self.time_B] * length
+        packet_dict["point_id"] = self.point_id.tolist()
+        packet_dict["loc_board_x"] = self.loc_board_x.tolist()
+        packet_dict["loc_board_y"] = self.loc_board_y.tolist()
+        packet_dict["loc_img_x_A"] = self.loc_img_x_A.tolist()
+        packet_dict["loc_img_y_A"] = self.loc_img_y_A.tolist()
+        packet_dict["loc_img_x_B"] = self.loc_img_x_B.tolist()
+        packet_dict["loc_img_y_B"] = self.loc_img_y_B.tolist()
+
+        return packet_dict
+
 
 if __name__ == "__main__":
     from src.recording.recorded_stream import RecordedStreamPool
@@ -162,6 +189,7 @@ if __name__ == "__main__":
     repo = Path(__file__).parent.parent.parent
     print(repo)
     session_directory = Path(repo, "sessions", "iterative_adjustment")
+    csv_output = Path(session_directory, "paired_point_data.csv")
 
     ports = [0, 1, 2]
     recorded_stream_pool = RecordedStreamPool(ports, session_directory)
@@ -177,9 +205,7 @@ if __name__ == "__main__":
     pairs = [(0, 1), (0, 2), (1, 2)]
 
     locatr = PairedPointStream(
-        synchronizer=syncr,
-        pairs=pairs,
-        tracker=trackr,
+        synchronizer=syncr, pairs=pairs, tracker=trackr, csv_output_path=csv_output
     )
 
     while True:

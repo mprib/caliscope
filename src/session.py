06723eb22e36572d100dc00e489ad88605514f01
mprib <prible@gmail.com>
2023-01-09 17:29:14 -0600

    typo fix

--- src/session.py
+++ src/session.py
@@ -270,7 +270,7 @@ class Session:
             logging.info("No stereo frame emitter to stop")
 
         try:
-            logging.info("Attemptidg to stop stereocalibrator")
+            logging.info("Attempting to stop stereocalibrator")
             self.stereocalibrator.stop()
 
         except (AttributeError):

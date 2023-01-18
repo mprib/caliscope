999f837765529aa55b3ea29ccb0637a16325c44b
mprib <31831778+mprib@users.noreply.github.com>
2023-01-18 15:49:36 -0600

    major updates to take PointData argument

--- src/calibration/bundle_adjustment/bundle_adjust_functions.py
+++ src/calibration/bundle_adjustment/bundle_adjust_functions.py
@@ -11,44 +11,51 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+from dataclasses import dataclass
 from scipy.optimize import least_squares
 from scipy.sparse import lil_matrix
 import time
 
 from src.cameras.camera_array import CameraArray, CameraArrayBuilder
 from src.calibration.bundle_adjustment.get_init_params import (
-    get_2d_3d_points,
+    PointData,
     get_camera_params,
 )
 
 
-# note I'm changing this from 9 as I'm reducing the values being solved for
 CAMERA_PARAM_COUNT = 6
 
 
 def xy_reprojection_error(
-    params, n_cameras, n_points, camera_indices, point_indices, points_2d, camera_array
+    current_param_estimates,
+    n_cameras,
+    n_points,
+    camera_indices,
+    point_indices,
+    points_2d,
+    camera_array,
 ):
     """
-    params: the current iteration of the vector that was originally initialized for the x0 input of least squares
+    current_param_estimates: the current iteration of the vector that was originally initialized for the x0 input of least squares
 
     """
 
     # Create one combined array primarily to make sure all calculations line up
-
-    ## convert current vectorized camera parameter estimates to matrix for ease of reference
-    camera_params = params[: n_cameras * CAMERA_PARAM_COUNT].reshape(
+    ## unpack the working estimates of the camera 6dof
+    camera_params = current_param_estimates[: n_cameras * CAMERA_PARAM_COUNT].reshape(
         (n_cameras, CAMERA_PARAM_COUNT)
     )
 
-    ## similarly convert the current vectorized 3d point estimates to matrix for ease of reference
-    points_3d = params[n_cameras * CAMERA_PARAM_COUNT :].reshape((n_points, 3))
+    ## similarly unpack the 3d points estimates
+    points_3d = current_param_estimates[n_cameras * CAMERA_PARAM_COUNT :].reshape(
+        (n_points, 3)
+    )
 
     ## created zero columns as placeholders for the reprojected 2d points
     rows = camera_indices.shape[0]
     blanks = np.zeros((rows, 2), dtype=np.float64)
 
-    ## hstack these arrays for ease of reference
+    ## hstack all these arrays for ease of reference
     points_3d_and_2d = np.hstack(
         [np.array([camera_indices]).T, points_3d[point_indices], points_2d, blanks]
     )
@@ -72,7 +79,8 @@ def xy_reprojection_error(
         points_3d_and_2d[cam_points, 6:8] = cam_proj_points[:, 0, :]
 
     points_proj = points_3d_and_2d[:, 6:8]
-    # points_proj = project(points_3d[point_indices], camera_params[camera_indices])
+
+    # reshape the x,y reprojection error to a single vector
     return (points_proj - points_2d).ravel()
 
 
@@ -97,52 +105,33 @@ def get_sparsity_pattern(n_cameras, n_points, camera_indices, point_indices):
     return A
 
 
-# TODO: #60 don't pass a csv_path...you can't really reuse this function if that is the case
-
-
-def bundle_adjust(
-    camera_array: CameraArray, camera_indices, point_indices, points_2d, points_3d
-):
+def bundle_adjust(camera_array: CameraArray, pts: PointData):
     # Original example taken from https://scipy-cookbook.readthedocs.io/items/bundle_adjustment.html
-
-    # MAC: start here tomorrow. You need to figure out how to cull the points
-    # based on reproj error and rerun bundle adjustment
-
     camera_params = get_camera_params(camera_array)
-
     n_cameras = camera_params.shape[0]
-    n_points = points_3d.shape[0]
 
-    n = CAMERA_PARAM_COUNT * n_cameras + 3 * n_points
-    m = 2 * points_2d.shape[0]
+    n_obj_points = pts.obj.shape[0]
+
+    n = CAMERA_PARAM_COUNT * n_cameras + 3 * n_obj_points
+    m = 2 * pts.img.shape[0]
 
     logging.info(f"n_cameras: {n_cameras}")
-    logging.info(f"n_points: {n_points}")
+    logging.info(f"n_points: {n_obj_points}")
     logging.info(f"Total number of parameters: {n}")
     logging.info(f"Total number of residuals: {m}")
 
-    initial_estimate = np.hstack((camera_params.ravel(), points_3d.ravel()))
-
-    # test the reprojection_error...here ahead of least squares for debugging purposes
-    # objective_value = xy_reprojection_error(
-    #     initial_estimate,
-    #     n_cameras,
-    #     n_points,
-    #     camera_indices,
-    #     point_indices,
-    #     points_2d,
-    #     camera_array,
-    # )
+    initial_param_estimate = np.hstack((camera_params.ravel(), pts.obj.ravel()))
 
     sparsity_pattern = get_sparsity_pattern(
-        n_cameras, n_points, camera_indices, point_indices
+        n_cameras, n_obj_points, pts.camera_indices, pts.obj_indices 
     )
 
     t0 = time.time()
     logging.info(f"Start time of bundle adjustment calculations is {t0}")
+
     optimized = least_squares(
         xy_reprojection_error,
-        initial_estimate,
+        initial_param_estimate,
         jac_sparsity=sparsity_pattern,
         verbose=2,
         x_scale="jac",
@@ -151,13 +140,14 @@ def bundle_adjust(
         method="trf",
         args=(
             n_cameras,
-            n_points,
-            camera_indices,
-            point_indices,
-            points_2d,
+            n_obj_points,
+            pts.camera_indices,
+            pts.obj_indices,
+            pts.img,
             camera_array,
         ),
     )
+
     t1 = time.time()
     logging.info(f"Completion time of bundle adjustment calculations is {t1}")
     logging.info(f"Total time to perform bundle adjustment: {t1-t0}")

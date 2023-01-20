a7d68b2645ba8d90b0fe6d3dbeef3050b90e644c
mprib <31831778+mprib@users.noreply.github.com>
2023-01-20 11:30:48 -0600

    encapsulate inputs in PointData

--- src/calibration/bundle_adjustment/bundle_adjust_functions.py
+++ src/calibration/bundle_adjustment/bundle_adjust_functions.py
@@ -24,43 +24,38 @@ CAMERA_PARAM_COUNT = 6
 
 def xy_reprojection_error(
     current_param_estimates,
-    n_cameras,
-    n_points,
-    camera_indices,
-    point_indices,
-    points_2d,
     camera_array,
+    point_data
 ):
     """
     current_param_estimates: the current iteration of the vector that was originally initialized for the x0 input of least squares
-
     """
 
     # Create one combined array primarily to make sure all calculations line up
     ## unpack the working estimates of the camera 6dof
-    camera_params = current_param_estimates[: n_cameras * CAMERA_PARAM_COUNT].reshape(
-        (n_cameras, CAMERA_PARAM_COUNT)
+    camera_params = current_param_estimates[: point_data.n_cameras * CAMERA_PARAM_COUNT].reshape(
+        (point_data.n_cameras, CAMERA_PARAM_COUNT)
     )
 
     ## similarly unpack the 3d points estimates
-    points_3d = current_param_estimates[n_cameras * CAMERA_PARAM_COUNT :].reshape(
-        (n_points, 3)
+    points_3d = current_param_estimates[point_data.n_cameras * CAMERA_PARAM_COUNT :].reshape(
+        (point_data.n_obj_points, 3)
     )
 
     ## created zero columns as placeholders for the reprojected 2d points
-    rows = camera_indices.shape[0]
+    rows = point_data.camera_indices.shape[0]
     blanks = np.zeros((rows, 2), dtype=np.float64)
 
     ## hstack all these arrays for ease of reference
     points_3d_and_2d = np.hstack(
-        [np.array([camera_indices]).T, points_3d[point_indices], points_2d, blanks]
+        [np.array([point_data.camera_indices]).T, points_3d[point_data.obj_indices], point_data.img, blanks]
     )
 
     # iterate across cameras...while this injects a loop in the residual function
     # it should scale linearly with the number of cameras...a tradeoff for stable
     # and explicit calculations...
     for port, cam in camera_array.cameras.items():
-        cam_points = np.where(camera_indices == port)
+        cam_points = np.where(point_data.camera_indices == port)
         object_points = points_3d_and_2d[cam_points][:, 1:4]
         rvec = camera_params[port][0:3]
         tvec = camera_params[port][3:6]
@@ -77,22 +72,22 @@ def xy_reprojection_error(
     points_proj = points_3d_and_2d[:, 6:8]
 
     # reshape the x,y reprojection error to a single vector
-    return (points_proj - points_2d).ravel()
+    return (points_proj - point_data.img).ravel()
 
 
 
 def bundle_adjust(camera_array: CameraArray, point_data: PointData):
     # Original example taken from https://scipy-cookbook.readthedocs.io/items/bundle_adjustment.html
     camera_params = camera_array.get_extrinsic_params()
-    n_cameras = camera_params.shape[0]
+    # n_cameras = camera_params.shape[0]
 
-    n_obj_points = point_data.obj.shape[0]
+    # n_obj_points = point_data.obj.shape[0]
 
-    n = CAMERA_PARAM_COUNT * n_cameras + 3 * n_obj_points
-    m = 2 * point_data.img.shape[0]
+    n = CAMERA_PARAM_COUNT * point_data.n_cameras + 3 * point_data.n_obj_points
+    m = 2 * point_data.n_img_points
 
-    logging.info(f"n_cameras: {n_cameras}")
-    logging.info(f"n_points: {n_obj_points}")
+    logging.info(f"n_cameras: {point_data.n_cameras}")
+    logging.info(f"n_points: {point_data.n_obj_points}")
     logging.info(f"Total number of parameters: {n}")
     logging.info(f"Total number of residuals: {m}")
 
@@ -113,12 +108,8 @@ def bundle_adjust(camera_array: CameraArray, point_data: PointData):
         ftol=1e-8,
         method="trf",
         args=(
-            n_cameras,
-            n_obj_points,
-            point_data.camera_indices,
-            point_data.obj_indices,
-            point_data.img,
             camera_array,
+            point_data,
         ),
     )
 

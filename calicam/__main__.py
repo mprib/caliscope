62e929b8cb7e38dd626c800aa29ffca2b80a6d1b
mprib <prible@gmail.com>
2023-01-29 17:22:34 -0600

    minor cleanup and simplification

--- calicam/__main__.py
+++ calicam/__main__.py
@@ -1,13 +1,3 @@
-# __main__.py
-import sys
-from pathlib import Path
-
-
-base_package_path = Path(__file__).parent.parent
-print(f"adding base_package_path: {base_package_path} : to sys.path")
-sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
 
 from calicam.gui.main import launch_main_window
 
-if __name__ == "__main__":
-    launch_main_window()

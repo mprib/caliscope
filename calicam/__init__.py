23f71e48d2a6fb933788f0c7e37c17b32a653eaa
mprib <prible@gmail.com>
2023-01-29 17:22:34 -0600

    minor cleanup and simplification

--- calicam/__init__.py
+++ calicam/__init__.py
@@ -9,8 +9,6 @@ __repo_owner_github_user_name__ = "mprib"
 __repo_url__ = f"https://github.com/{__repo_owner_github_user_name__}/{__package_name__}/"
 __repo_issues_url__ = f"{__repo_url__}issues"
 
-import sys
-from pathlib import Path
 
 print(f"Thank you for using {__package_name__}!")
 print(f"This is printing from: {__file__}")
@@ -18,6 +16,5 @@ print(f"Source code for this package is available at: {__repo_url__}")
 
 from calicam.system.default_paths import get_log_file_path
 from calicam.system.logging_configuration import configure_logging
-
 
 configure_logging(log_file_path=get_log_file_path())

550e23ecac87770f71956a9ab989aea4766c75dc
mprib <prible@gmail.com>
2023-01-31 17:55:26 -0600

    update logging

--- calicam/gui/camera_config/camera_tabs.py
+++ calicam/gui/camera_config/camera_tabs.py
@@ -1,10 +1,6 @@
-import logging
 
-LOG_FILE = "log/camera_tabs.log"
-LOG_LEVEL = logging.DEBUG
-LOG_FORMAT = " %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s"
-
-logging.basicConfig(filename=LOG_FILE, filemode="w", format=LOG_FORMAT, level=LOG_LEVEL)
+import calicam.logger
+logger = calicam.logger.get(__name__)
 
 import sys
 from pathlib import Path

cf4bd99ead5758b58c2e89b4eef4e4de93071eb7
mprib <31831778+mprib@users.noreply.github.com>
2023-02-03 16:50:07 -0600

    basic working folder select for wizard working now

---
+++ calicam/gui_wizard/wizard_main.py
@@ -0,0 +1,25 @@
+
+
+import calicam.logger
+logger = calicam.logger.get(__name__)
+
+import sys
+from PyQt6.QtWidgets import QApplication, QWizard, QWizardPage, QLabel, QVBoxLayout, QRadioButton, QButtonGroup
+
+from calicam.gui_wizard.wizard_intro import WizardIntro
+class WizardMain(QWizard):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Calibration Wizard")
+
+        intro_page = WizardIntro()
+        self.addPage(intro_page)
+
+
+
+if __name__ == "__main__":
+    
+    app = QApplication(sys.argv)
+    wizard = WizardMain()
+    wizard.show()
+    sys.exit(app.exec())

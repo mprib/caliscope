57a45f94cc39e24a1aa209b34525a5c12d55e571
mprib <prible@gmail.com>
2023-02-04 15:55:50 -0600

    attempting different structure of dialogs

--- calicam/gui_wizard/wizard_main.py
+++ calicam/gui_wizard/wizard_main.py
@@ -13,21 +13,28 @@ from PyQt6.QtWidgets import (
     QButtonGroup,
 )
 
-from calicam.gui_wizard.wizard_intro import WizardIntro
-
-
+from calicam.gui_wizard.wiz_get_paths import WizardIntro
+from calicam.gui_wizard.wizard_charuco import WizardCharuco
+from calicam.session import Session
 class WizardMain(QWizard):
-    def __init__(self):
+    def __init__(self, config_path):
         super().__init__()
         self.setWindowTitle("Calibration Wizard")
+        self.config_path = config_path
+        self.session = Session(config_path)
 
-        intro_page = WizardIntro()
-        self.addPage(intro_page)
+        self.intro_page = WizardIntro()
+        charuco_page = WizardCharuco(self.session)
+        self.addPage(charuco_page) 
 
 
 if __name__ == "__main__":
+    from pathlib import Path
+    
+    repo = Path(str(Path(__file__)).split("calicam")[0], "calicam")
+    config_path = Path(repo, "sessions", "high_res_session")
 
     app = QApplication(sys.argv)
-    wizard = WizardMain()
+    wizard = WizardMain(config_path)
     wizard.show()
     sys.exit(app.exec())

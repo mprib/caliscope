69bca077453721d0bced57724880cbf0a53ceaaa
mprib <prible@gmail.com>
2023-02-05 13:52:48 -0600

    new versions of cam config for wizard

---
+++ calicam/gui_wizard/wizard_camera_config/camera_tabs.py
@@ -0,0 +1,70 @@
+
+import calicam.logger
+logger = calicam.logger.get(__name__)
+
+import sys
+from pathlib import Path
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QTabWidget,
+)
+
+from calicam.gui.camera_config.camera_config_dialogue import CameraConfigDialog
+from calicam.session import Session
+
+class CameraTabs(QTabWidget):
+    
+    def __init__(self, session):
+        super(CameraTabs, self).__init__()
+        self.session = session
+
+        self.setTabPosition(QTabWidget.TabPosition.North)
+        self.add_cam_tabs()
+
+    def add_cam_tabs(self):
+        tab_names = [self.tabText(i) for i in range(self.count())]
+        logger.info(f"Current tabs are: {tab_names}")
+
+        if len(self.session.monocalibrators) > 0:
+            for port, monocal in self.session.monocalibrators.items():
+                tab_name = f"Camera {port}"
+                logger.info(f"Potentially adding {tab_name}")
+                if tab_name in tab_names:
+                    pass  # already here, don't bother
+                else:
+                    cam_tab = CameraConfigDialog(self.session, port)
+
+                    # def on_save_click():
+                    #     self.summary.camera_table.update_data()
+
+                    # cam_tab.save_cal_btn.clicked.connect(on_save_click)
+                    # move code below to main.py
+                    # cam_tab.save_cal_btn.clicked.connect(self.summary.camera_table.update_data)
+
+                    self.insertTab(port, cam_tab, tab_name)
+        else:
+            logger.info("No cameras available")
+
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+
+    repo = Path(str(Path(__file__)).split("calicam")[0],"calicam").parent
+    config_path = Path(repo, "sessions", "high_res_session")
+    print(config_path)
+    session = Session(config_path)
+    session.load_cameras()
+    session.load_streams()
+    # session.adjust_resolutions()
+    session.load_monocalibrators()
+
+    test_port = 0
+
+    # cam_dialog = CameraConfigDialog(session, test_port)
+    cam_tabs = CameraTabs(session)
+    
+    cam_tabs.show()
+
+    sys.exit(App.exec())
+

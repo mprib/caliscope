b3ed9d48be969197d7b284bd2e27e5607aaa4eec
mprib <prible@gmail.com>
2023-02-21 10:16:25 -0600

    building out sync packet recreator

--- calicam/calibration/capture_volume/sync_packet_builder.py
+++ calicam/calibration/capture_volume/sync_packet_builder.py
@@ -7,8 +7,11 @@ from itertools import combinations
 import pandas as pd
 import numpy as np
 from pathlib import Path
+import time
+
 from calicam import __root__
 from calicam.cameras.data_packets import PointPacket, FramePacket, SyncPacket
+from calicam.triangulate.paired_point_builder import PairedPointBuilder
 #%%
 
 point_data_path = Path(__root__, "tests", "5_cameras", "recording", "point_data.csv")
@@ -24,37 +27,51 @@ xy_img = np.vstack([img_x,img_y]).T
 
 
 ports = np.unique(xy_camera_indices)
-sync_indices = np.unique(xy_sync_indices)
 
+paired_point_builder = PairedPointBuilder(ports)
+
+sync_indices = np.unique(xy_sync_indices)
 pairs = [(i,j) for i,j in combinations(ports,2) if i<j]
 
-for sync_index in [0,1,2,3]: # sync_indices:
+
+#%% 
+print(time.time())
+for sync_index in [0,1,2,3]: 
+# for sync_index in sync_indices:
     port_points = point_data.query(f"sync_index == {sync_index}")
 
-    ports = port_points["port"].unique()
-    print(f"{sync_index}: {ports}")
+    # ports = port_points["port"].unique()
+    # print(f"{sync_index}: {ports}")
 
     frame_packets = {}
 
     for port in ports:
-        points = port_points.query(f"port == {port}")
-        frame_time = points["frame_time"].iloc[0]
-        frame_index = points["frame_index"].iloc[0]
+        
+        if port in port_points["port"].unique():
+            points = port_points.query(f"port == {port}")
+            frame_time = points["frame_time"].iloc[0]
+            frame_index = points["frame_index"].iloc[0]
 
-        point_id = points["point_id"]
+            point_id = points["point_id"].to_numpy()
 
-        img_loc_x = points["img_loc_x"].to_numpy()        
-        img_loc_y = points["img_loc_y"].to_numpy()        
-        img_loc = np.vstack([img_loc_x,img_loc_y]).T
+            img_loc_x = points["img_loc_x"].to_numpy()        
+            img_loc_y = points["img_loc_y"].to_numpy()        
+            img_loc = np.vstack([img_loc_x,img_loc_y]).T
 
-        board_loc_x = points["board_loc_x"].to_numpy()        
-        board_loc_y = points["board_loc_y"].to_numpy()        
-        board_loc = np.vstack([board_loc_x,board_loc_y]).T
+            board_loc_x = points["board_loc_x"].to_numpy()        
+            board_loc_y = points["board_loc_y"].to_numpy()        
+            board_loc = np.vstack([board_loc_x,board_loc_y]).T
 
-        point_packet = PointPacket(point_id, img_loc, board_loc)
-        frame_packet = FramePacket(port, frame_time, None, frame_index, point_packet)
-        frame_packets[port] = frame_packet
+            point_packet = PointPacket(point_id, img_loc, board_loc)
+            frame_packet = FramePacket(port, frame_time, None, frame_index, point_packet)
+            frame_packets[port] = frame_packet
+        else:
+            frame_packets[port] = None
         
     sync_packet = SyncPacket(sync_index, frame_packets)        
 
+    synched_paired_points = paired_point_builder.get_synched_paired_points(sync_packet)
+    # print(synched_paired_points)
+    
+print(time.time())
 # %%

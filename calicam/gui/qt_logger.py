5dff090d197d4107c32cfb2174c58dbe1ef3430e
mprib <prible@gmail.com>
2023-02-27 16:30:01 -0600

    autoscroll functionality is now working...

--- calicam/gui/qt_logger.py
+++ calicam/gui/qt_logger.py
@@ -17,27 +17,49 @@ def test():
 from calicam.logger import get, XStream
 logger = get(__name__)
 
-class LoggerPopUp(QDialog):
+class QtLogger(QDialog):
     def __init__( self, parent = None ):
-        super(LoggerPopUp, self).__init__(parent)
+        super(QtLogger, self).__init__(parent)
 
-        self._console = QTextBrowser(self)
+        self._console = LogMessageViewer(self)
+
         self._button  = QPushButton(self)
         self._button.setText('Test Me')
+        self.vertical_scroll_bar = self._console.verticalScrollBar()
 
         layout = QVBoxLayout()
         layout.addWidget(self._console)
         layout.addWidget(self._button)
-        self.setLayout(layout)
 
-        XStream.stdout().messageWritten.connect( self._console.insertPlainText )
-        XStream.stderr().messageWritten.connect( self._console.insertPlainText )
+        self.setLayout(layout)
+        XStream.stdout().messageWritten.connect( self._console.appendLogMessage)
+        XStream.stderr().messageWritten.connect( self._console.appendLogMessage)
 
         self._button.clicked.connect(test)
 
+class LogMessageViewer(QTextBrowser):
+
+    def __init__(self, parent=None):
+        super(LogMessageViewer,self).__init__(parent)
+        self.setReadOnly(True)
+        #self.setLineWrapMode(QtGui.QTextEdit.NoWrap)
+
+
+    @QtCore.pyqtSlot(str)
+    def appendLogMessage(self, msg):
+        horScrollBar = self.horizontalScrollBar()
+        verScrollBar = self.verticalScrollBar()
+        scrollIsAtEnd = verScrollBar.maximum() - verScrollBar.value() <= 10
+
+        self.insertPlainText(msg)
+
+        if scrollIsAtEnd:
+            verScrollBar.setValue(verScrollBar.maximum()) # Scrolls to the bottom
+            horScrollBar.setValue(0) # scroll to the left
+    
 if __name__ == '__main__':
     app = QApplication([])
-    dlg = LoggerPopUp()
+    dlg = QtLogger()
     dlg.show()
         
     app.exec()

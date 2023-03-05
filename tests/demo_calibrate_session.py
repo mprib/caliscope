62e3dee399110b464d632768d981bc78c9f07977
mprib <prible@gmail.com>
2023-03-04 20:22:52 -0600

    new test

--- tests/demo_calibrate_session.py
+++ tests/demo_calibrate_session.py
@@ -12,7 +12,7 @@ from calicam.calibration.capture_volume.helper_functions.get_point_estimates imp
 
 # the session used for the single camera calibration and 
 # the omniframe data collection
-session_directory = Path(__root__,"tests", "mimic_anipose" )
+session_directory = Path(__root__,"tests", "please work" )
 
 # point_data.csv is created during the omniframe datacollection
 point_data_path = Path(session_directory, "point_data.csv")

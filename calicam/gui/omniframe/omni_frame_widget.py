9da239940954f30e62b7a8bc6b610399fe2b45a3
mprib <prible@gmail.com>
2023-03-05 16:23:56 -0600

    skeleton of omniframe widget

--- calicam/gui/omniframe/omni_frame_widget.py
+++ calicam/gui/omniframe/omni_frame_widget.py
@@ -29,6 +29,7 @@ from PyQt6.QtWidgets import (
 # Append main repo to top of path to allow import of backend
 from calicam.session import Session
 from calicam.gui.omniframe.omni_frame_builder import OmniFrameBuilder
+from calicam.cameras.synchronizer import Synchronizer
 from calicam import __root__
 
 
@@ -38,76 +39,93 @@ class OmniFrameWidget(QWidget):
 
         super(OmniFrameWidget, self).__init__()
         self.session = session
-        self.synchronizer = self.session.get_synchronizer()
+        self.synchronizer:Synchronizer = self.session.get_synchronizer()
 
         self.frame_builder = OmniFrameBuilder(self.synchronizer)
         self.frame_emitter = OmniFrameEmitter(self.frame_builder)
+        self.frame_emitter.start()
 
         self.layout_widgets()
         self.connect_widgets()        
 
     def layout_widgets(self):
         
-        self.setLayout(QHBoxLayout())
-        omni_frame_display = QLabel()
-        self.layout().addWidget(omni_frame_display)
+        self.setLayout(QVBoxLayout())
+        self.omni_frame_display = QLabel()
+        self.layout().addWidget(self.omni_frame_display)
 
     def connect_widgets(self):
 
-        def ImageUpdateSlot(QPixmap):
-            self.omni_frame_display.setPixmap(QPixmap)
 
-        self.frame_emitter.ImageBroadcast.connect(ImageUpdateSlot)
+        self.frame_emitter.ImageBroadcast.connect(self.ImageUpdateSlot)
         
 
+    def ImageUpdateSlot(self, q_image):
+        logger.info("using slot")
+        qpixmap = QPixmap.fromImage(q_image)
+        logger.info("about to set pixmap")
+        self.omni_frame_display.setPixmap(qpixmap)
 
 
 
 class OmniFrameEmitter(QThread):
-    ImageBroadcast = pyqtSignal(QPixmap)
+    ImageBroadcast = pyqtSignal(QImage)
     
     def __init__(self, omniframe_builder:OmniFrameBuilder):
         
         super(OmniFrameEmitter,self).__init__()
         self.omniframe_builder = omniframe_builder
-        
+        logger.info("Initiated frame emitter")        
         
     def run(self):
-        
+        logger.info("ENTERING LOOP")
         while True:
+            logger.info("Looping at emitter")
             omni_frame = self.omniframe_builder.get_omni_frame()
-            image = cv2_to_qlabel(omni_frame)
-            
-            self.ImageBroadcast.emit(image)
-    
+            cv2.imshow("omni Frame", omni_frame)
+
+            # logger.info("Successfully pulled omni frame")
+
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                cv2.destroyAllWindows()
+                break
+            if omni_frame is not None:
+                logger.info("convert to qlabel")
+                image = cv2_to_qlabel(omni_frame)
+                logger.info("convert to pixmap")
+
+                logger.info("broadcast pixmap")
+                self.ImageBroadcast.emit(image)
     
     
 def cv2_to_qlabel(frame):
-    Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FlippedImage = cv2.flip(Image, 1)
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     qt_frame = QImage(
-        FlippedImage.data,
-        FlippedImage.shape[1],
-        FlippedImage.shape[0],
+        image.data,
+        image.shape[1],
+        image.shape[0],
         QImage.Format.Format_RGB888,
     )
     return qt_frame
 
     
 if __name__ == "__main__":
-    
-    App = QApplication(sys.argv)
+   
+   
+   
+        App = QApplication(sys.argv)
 
-    config_path = Path(__root__, "tests", "please work")
+        config_path = Path(__root__, "tests", "please work")
 
-    session = Session(config_path)
-    session.load_cameras()
-    session.load_streams()
-    session.adjust_resolutions()
+        session = Session(config_path)
+        session.load_cameras()
+        session.load_streams()
+        # session.adjust_resolutions()
 
 
-    omni_dialog = OmniFrameWidget(session)
-    omni_dialog.show()
+        omni_dialog = OmniFrameWidget(session)
+        omni_dialog.show()
 
-    sys.exit(App.exec())
+        sys.exit(App.exec())

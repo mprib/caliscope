9c84bb44796326c44e8ff153caf97f83e450df55
mprib <prible@gmail.com>
2023-03-15 07:17:12 -0500

    winning test written

--- tests/demo_calibrate_session.py
+++ tests/demo_calibrate_session.py
@@ -1,18 +1,27 @@
 #%%
 from pathlib import Path
+from PyQt6.QtWidgets import QApplication
+import sys
 
 from pyxyfy.session import Session
 from pyxyfy import __root__
 from pyxyfy.calibration.omnicalibrator import OmniCalibrator
 from pyxyfy.calibration.capture_volume.point_estimates import PointEstimates
 
+from pyxyfy.calibration.capture_volume.capture_volume import CaptureVolume
 from pyxyfy.calibration.capture_volume.helper_functions.get_point_estimates import (
     get_point_estimates,
 )
 
+from pyxyfy.gui.vizualize.capture_volume_visualizer import CaptureVolumeVisualizer
+
 # the session used for the single camera calibration and 
 # the omniframe data collection
-session_directory = Path(__root__,"tests", "pyxyfy" )
+# session_directory = Path(__root__,"tests", "3_cameras_linear" )
+
+
+# NOTE: This will successfully calibrate when I fix my issue
+session_directory = Path(__root__,"tests", "3_cameras_triangular" )
 
 # point_data.csv is created during the omniframe datacollection
 point_data_path = Path(session_directory, "point_data.csv")
@@ -49,7 +58,6 @@ point_estimates: PointEstimates = get_point_estimates(
 session.save_camera_array()
 
 #%%
-from pyxyfy.calibration.capture_volume.capture_volume import CaptureVolume
 capture_volume = CaptureVolume(session.camera_array, point_estimates)
 
 capture_volume.save(session_directory)
@@ -59,3 +67,7 @@ capture_volume.optimize()
 # %%
 session.save_camera_array()
 capture_volume.save(session_directory)
+
+app = QApplication(sys.argv)
+vizr = CaptureVolumeVisualizer(capture_volume = capture_volume)
+sys.exit(app.exec())

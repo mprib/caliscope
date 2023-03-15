fd25904871beab51a1aaf48d53a00383101ea1ba
mprib <31831778+mprib@users.noreply.github.com>
2023-03-15 16:07:19 -0500

    testing on laptop

--- pyxyfy/gui/camera_config/camera_tabs.py
+++ pyxyfy/gui/camera_config/camera_tabs.py
@@ -102,8 +102,8 @@ if __name__ == "__main__":
     App = QApplication(sys.argv)
 
     
-    # config_path = Path(__root__, "sessions", "laptop")
-    config_path = Path(__root__, "tests", "pyxyfy")
+    config_path = Path(__root__, "tests", "laptop")
+    # config_path = Path(__root__, "tests", "pyxyfy")
     # config_path = Path(repo, "sessions", "high_res_session")
     print(config_path)
     session = Session(config_path)

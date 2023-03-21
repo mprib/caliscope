ff558e741cf3c21cd24efc88f58bc651a384762f
mprib <prible@gmail.com>
2023-03-21 12:11:01 -0500

    slider across collected points working

--- pyxy3d/gui/vizualize/capture_volume_dialog.py
+++ pyxy3d/gui/vizualize/capture_volume_dialog.py
@@ -26,62 +26,76 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-from pyxy3d.gui.stereo_calibration.stereo_pair_widget import StereoPairWidget
-from pyxy3d.gui.capture_volume.visualizer import CaptureVolumeVisualizer
+from pyxy3d.gui.vizualize.capture_volume_visualizer import CaptureVolumeVisualizer
 
 class CaptureVolumeDialog(QWidget):
     def __init__(self, CaptureVolumeVisualizer):
         super(CaptureVolumeDialog, self).__init__()
         self.visualizer = CaptureVolumeVisualizer
-        self.setLayout(QVBoxLayout())
-        self.layout().addWidget(QLabel("This is a test"))
-        self.layout().addWidget(self.visualizer.scene)
+        # self.visualizer.scene.show()
+        self.slider = QSlider(Qt.Orientation.Horizontal)
+        self.slider.setMinimum(self.visualizer.min_sync_index)
+        self.slider.setMaximum(self.visualizer.max_sync_index)
+        self.set_origin_btn = QPushButton("Set Origin")
+
         self.setMinimumSize(500,500)
-        self.visualizer.begin()
         
+        self.place_widgets()
+        self.connect_widgets()
+
+        self.visualizer.display_points(self.visualizer.min_sync_index)
+        
+        
+    def place_widgets(self):
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.visualizer.scene)
+        self.layout().addWidget(self.slider)
+        self.layout().addWidget(self.set_origin_btn)
+        # self.visualizer.begin()
+
+    def connect_widgets(self):
+        self.slider.valueChanged.connect(self.visualizer.display_points)
+
+
+    def update_board(self, sync_index):
+        
+        logger.info(f"Updating board to sync index {sync_index}")
+        
+        self.visualizer.display_points(sync_index)
 if __name__ == "__main__":
 
-    from pyxy3d.recording.recorded_stream import RecordedStreamPool
-    from pyxy3d.cameras.synchronizer import Synchronizer
-    from pyxy3d.calibration.charuco import Charuco
-    from pyxy3d.calibration.corner_tracker import CornerTracker
-    from pyxy3d.triangulate.stereo_points_builder import StereoPointsBuilder
-    from pyxy3d.triangulate.stereo_triangulator import StereoTriangulator
-    from pyxy3d.triangulate.stereo_triangulator import StereoTriangulator
+    from PyQt6.QtWidgets import QApplication
 
-    # set the location for the sample data used for testing
-    repo = Path(str(Path(__file__)).split("pyxy")[0],"pyxy").parent
-    session_directory =Path(repo, "sessions", "high_res_session")
-    # create playback streams to provide to synchronizer
-    ports = [0, 2]
-    recorded_stream_pool = RecordedStreamPool(ports, session_directory)
-    syncr = Synchronizer(recorded_stream_pool.streams, fps_target=None)
-    recorded_stream_pool.play_videos()
-    # create a corner tracker to locate board corners
-    charuco = Charuco(
-        4, 5, 11, 8.5, aruco_scale=0.75, square_size_overide_cm=5.25, inverted=True
-    )
-    trackr = CornerTracker(charuco)
+    from pyxy3d import __root__
+    from pyxy3d.cameras.camera_array_builder_deprecate import CameraArrayBuilder
+    from pyxy3d.calibration.capture_volume.helper_functions.get_point_estimates import get_point_estimates
 
-    # create a commmon point finder to grab charuco corners shared between the pair of ports
-    pairs = [(ports[0], ports[1])]
-    point_stream = StereoPointsBuilder(
-        synchronizer=syncr,
-        pairs=pairs,
-        tracker=trackr,
-
-    )
+    from pyxy3d.calibration.capture_volume.capture_volume import CaptureVolume
+    import pickle
+    
+    # session_directory = Path(__root__,  "tests", "2_cameras_linear")
+    # session_directory = Path(__root__,  "tests", "tripod")
+    # session_directory = Path(__root__,  "tests", "2_cameras_90_deg")
+    # session_directory = Path(__root__,  "tests", "2_cameras_180_deg")
+    # session_directory = Path(__root__,  "tests", "3_cameras_triangular")
+    # session_directory = Path(__root__,  "tests", "3_cameras_middle")
+    session_directory = Path(__root__,  "tests", "4_cameras_beginning")
+    # session_directory = Path(__root__,  "tests", "4_cameras_nonoverlap")
+    # session_directory = Path(__root__,  "tests", "3_cameras_linear")
+    # session_directory = Path(__root__,  "tests", "3_cameras_midlinear")
+    # session_directory = Path(__root__,  "tests", "just_checking")
 
 
-    config_path = str(Path(session_directory, "config.toml"))
-    triangulatr = StereoTriangulator(point_stream, config_path)
+    saved_CV_path = Path(session_directory, "capture_volume_stage_1.pkl") 
+    with open(saved_CV_path, "rb") as f:
+        capture_volume:CaptureVolume = pickle.load(f)
 
     app = QApplication(sys.argv)
+    vizr = CaptureVolumeVisualizer(capture_volume = capture_volume)
+    # vizr = CaptureVolumeVisualizer(camera_array = capture_volume.camera_array)
 
-    vizr = CaptureVolumeVisualizer(triangulatr)
-    vizr.add_point_q(triangulatr.out_q)
 
     vizr_dialog = CaptureVolumeDialog(vizr)
     vizr_dialog.show()
 
-    app.exec()
+    sys.exit(app.exec())

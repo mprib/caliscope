3b8e434878fe41ba3c9e24c7ce2b944ebb028b77
mprib <31831778+mprib@users.noreply.github.com>
2023-03-22 12:12:49 -0500

    getting charuco img points

--- tests/test_set_origin.py
+++ tests/test_set_origin.py
@@ -8,13 +8,15 @@ import numpy as np
 
 from pyxy3d import __root__
 from pyxy3d.calibration.capture_volume.capture_volume import CaptureVolume
-from pyxy3d.calibration.capture_volume.helper_functions.get_point_estimates import \
-    get_point_estimates
-from pyxy3d.calibration.capture_volume.point_estimates import PointEstimates    
+from pyxy3d.calibration.capture_volume.helper_functions.get_point_estimates import (
+    get_point_estimates,
+)
+from pyxy3d.calibration.capture_volume.point_estimates import PointEstimates
 from pyxy3d.cameras.camera_array_initializer import CameraArrayInitializer
 from pyxy3d.session import Session
 from pyxy3d.calibration.charuco import Charuco
-from cv2 import aruco
+from pyxy3d.cameras.camera_array import CameraData
+import cv2
 
 session_directory = Path(__root__, "tests", "4_cameras_endofday")
 
@@ -32,36 +34,46 @@ capture_volume.save(session_directory)
 capture_volume.optimize()
 capture_volume.save(session_directory)
 
-# %%
 
 # config_path = Path(session_directory, "config.toml")
 session = Session(session_directory)
 charuco_board = session.charuco.board
 
-# %%
 sync_indices = point_estimates.sync_indices
 test_sync_index = sync_indices[3]
 
-charuco_ids = point_estimates.point_id[sync_indices==test_sync_index]
-unique_charucos = np.unique(charuco_ids)
-unique_charucos.sort()
+charuco_ids = point_estimates.point_id[sync_indices == test_sync_index]
+unique_charuco_id = np.unique(charuco_ids)
+unique_charuco_id.sort()
 
 # pull out the 3d point estimate indexes associated with the chosen sync_index
 # note that this will include duplicates
-obj_indices = point_estimates.obj_indices[sync_indices ==test_sync_index]
+obj_indices = point_estimates.obj_indices[sync_indices == test_sync_index]
 # now get the actual x,y,z estimate associated with these unique charucos
 obj_xyz = point_estimates.obj[obj_indices]
 sorter = np.argsort(charuco_ids)
-unique_charuco_xyz_index = sorter[np.searchsorted(charuco_ids,unique_charucos, sorter = sorter)]
+unique_charuco_xyz_index = sorter[
+    np.searchsorted(charuco_ids, unique_charuco_id, sorter=sorter)
+]
 # need to get charuco ids associated with the 3 point positions
 unique_charuco_xyz = obj_xyz[unique_charuco_xyz_index]
+#%%
+# Convert 3d coordinates into 2d camera coordinates. Just pick a camera:
+anchor_camera: CameraData = camera_array.cameras[list(camera_array.cameras.keys())[0]]
 
+charuco_image_points, jacobian = cv2.projectPoints(
+    unique_charuco_xyz,
+    anchor_camera.rotation,
+    anchor_camera.translation,
+    anchor_camera.matrix,
+    distCoeffs = np.array([0,0,0,0,0], dtype = np.float64) # For origin setting, assume perfection
+)
 
 # Here is the plan: from a given sync_index, find which camera has the most points represented on it.
 # or wait...does this matter...can I just project back to the camera from the 3d points
 
 # copied from https://longervision.github.io/2017/03/12/ComputerVision/OpenCV/opencv-external-posture-estimation-ArUco-board/
-# retval, rvec, tvec = aruco.estimatePoseCharucoBoard(corners, ids, board, camera_matrix, dist_coeffs)  # posture estimation from a diamond
+# retval, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(corners, ids, board, camera_matrix, dist_coeffs)  # posture estimation from a diamond
 # Note for tomorrow: this is going to be more challenging than I'd thought..
 # board pose is estimated from each camera...
 # may need to get pose from each camera, then convert to a common

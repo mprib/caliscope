bcab52d08e750dd19859edd7703759790b610a31
mprib <prible@gmail.com>
2023-03-25 16:09:35 -0500

    have no idea at this point

--- tests/test_set_origin.py
+++ tests/test_set_origin.py
@@ -27,7 +27,7 @@ from pyxy3d.gui.vizualize.capture_volume_visualizer import CaptureVolumeVisualiz
 from pyxy3d.gui.vizualize.capture_volume_dialog import CaptureVolumeDialog
 import pickle
 
-session_directory = Path(__root__, "tests", "4_cameras_endofday")
+session_directory = Path(__root__, "tests", "4_cameras_beginning")
 point_data_csv_path = Path(session_directory, "point_data.csv")
 config_path = Path(session_directory, "config.toml")
 
@@ -60,10 +60,9 @@ charuco_board = session.charuco.board
 
 sync_indices = point_estimates.sync_indices
 # test_sync_index = sync_indices[46]
+test_sync_index = 320
 
 
-### Get target and actual board corners
-test_sync_index = 50
 
 charuco_ids = point_estimates.point_id[sync_indices == test_sync_index]
 unique_charuco_id = np.unique(charuco_ids)
@@ -80,74 +79,93 @@ unique_charuco_xyz_index = sorter[
     np.searchsorted(charuco_ids, unique_charuco_id, sorter=sorter)
 ]
 
+
+# corner positions in the estimated world position
 world_corners_xyz = obj_xyz[unique_charuco_xyz_index]
-# need to get x,y,z estimates in board world...
+# x,y,z positions in board world...
 board_corners_xyz = charuco_board.chessboardCorners[unique_charuco_id]
 #%%
-# get which axis is longest
-max_xyz = np.max(board_corners_xyz,axis=0)
-max_dim_value = np.max(max_xyz)
-# want to find the longest dim, x or y. if equal this will just choose dim 1 (x)
-longest_dim = np.where(max_xyz==max_dim_value)[0][0]
-# that longest dimension is where things will get cut in half
-# find the indexes from the board world that should fall into each half (A and B)
-centroid_indexes_A = np.where(board_corners_xyz[:,longest_dim] <= max_dim_value/2)
-centroid_indexes_B = np.where(board_corners_xyz[:,longest_dim] > max_dim_value/2)
+def get_centroid_distances(board_corners_xyz, world_corners_xyz):
+    # get which axis is longest
+    max_xyz = np.max(board_corners_xyz, axis=0)
+    max_dim_value = np.max(max_xyz)
+    # want to find the longest dim, x or y. if equal this will just choose dim 1 (x)
+    longest_dim = np.where(max_xyz == max_dim_value)[0][0]
+    # that longest dimension is where things will get cut in half
+    # find the indexes from the board world that should fall into each half (A and B)
+    centroid_indexes_A = np.where(
+        board_corners_xyz[:, longest_dim] <= max_dim_value / 2
+    )
+    centroid_indexes_B = np.where(board_corners_xyz[:, longest_dim] > max_dim_value / 2)
 
-#%%
-# slice up the world and board corners into the centroids
-board_centroid_A = np.mean(board_corners_xyz[centroid_indexes_A], axis=0)
-board_centroid_B = np.mean(board_corners_xyz[centroid_indexes_B], axis=0)
-world_centroid_A = np.mean(world_corners_xyz[centroid_indexes_A], axis=0)
-world_centroid_B = np.mean(world_corners_xyz[centroid_indexes_B], axis=0)
+    #%%
+    # slice up the world and board corners into the centroids
+    board_centroid_A = np.mean(board_corners_xyz[centroid_indexes_A], axis=0)
+    board_centroid_B = np.mean(board_corners_xyz[centroid_indexes_B], axis=0)
+    world_centroid_A = np.mean(world_corners_xyz[centroid_indexes_A], axis=0)
+    world_centroid_B = np.mean(world_corners_xyz[centroid_indexes_B], axis=0)
+
+    # check centroid distances and scale as needed
+
+    distance_world = np.sqrt(np.sum((world_centroid_B - world_centroid_A) ** 2))
+    distance_board = np.sqrt(np.sum((board_centroid_B - board_centroid_A) ** 2))
+
+    # correct target board centroid distances to avoid tilting for forced fit
+    correction_ratio = distance_world / distance_board
+    logger.info(f"Correction Ratio: {correction_ratio}")
+    board_centroid_A = board_centroid_A * correction_ratio
+    board_centroid_B = board_centroid_B * correction_ratio
+   
+   
+    # check scaling works
+    distance_world = np.sqrt(np.sum((world_centroid_B - world_centroid_A) ** 2))
+    distance_board = np.sqrt(np.sum((board_centroid_B - board_centroid_A) ** 2))
+    correction_ratio = distance_world / distance_board
+    logger.info(f"Correction Ratio after attempting to correct: {correction_ratio}")
+    
+     
+    centroid_A_distance = np.sqrt(np.sum(board_centroid_A - world_centroid_A)**2)
+    centroid_B_distance = np.sqrt(np.sum(board_centroid_B - world_centroid_B)**2)
+    
+    logger.info(f"Board Centroid A: {board_centroid_A}")
+    logger.info(f"World Centroid A: {world_centroid_A}")
+
+    logger.info(f"Board Centroid B: {board_centroid_B}")
+    logger.info(f"World Centroid B: {world_centroid_B}")
+    
+    delta_A = world_centroid_A - board_centroid_A
+    delta_B = world_centroid_B - board_centroid_B
+    return centroid_A_distance, centroid_B_distance
 
 
 
-#%%
-# quick check of corner distances in world and board frame to make sure I'm not
-# completely off track....
-test_index_A = 0
-test_index_B = 1
-
-# get the distance between them
-distance_world_A_B = np.sqrt(
-    np.sum((world_corners_xyz[test_index_A,:] - world_corners_xyz[test_index_B,:]) ** 2)
-)
-
-distance_board_A_B = np.sqrt(
-    np.sum((board_corners_xyz[test_index_A,:] - board_corners_xyz[test_index_B,:]) ** 2)
-)
-
-world_board_ratio = distance_world_A_B/distance_board_A_B
-# adjust board_corners_xyz to reflect the scale of the world
-board_corners_xyz = world_board_ratio*board_corners_xyz
-
-#%%
-# if True:
-
-def board_fit_error(six_dof_params, board_corners_xyz, world_corners_xyz ):
+def board_fit_error(six_dof_params, board_corners_xyz, world_corners_xyz):
     """
     returns a vector of values to minimize. These represent two different categories
     of positional information:
-    
+
     I - estimated z coordinates: in a perfect world, the z positions of the board will be zero,
     therefore these are provided as is with the target of pushing them to zero (i.e. making
     board flat)
-    
+
     This will only pin down the z values, therefore the x,y position must also be pinned down.
-    While one or the other is easy to pin down with a single corner, mild errors in the scale of 
+    While one or the other is easy to pin with a single corner, it can rotate around this corner.
+    Pinning a an aditional corner leads to complications as mild errors in the scale of
     the world can force the optimizer to push the board on a tilt in order to minimize the overall
     error (therefore pushing the z values away from zero).
-    
-    A solution to this is to fit the board to only two additional points that can be easily scaled 
-    to fit precisely. This is acheived by converting all board points into two centroid values
-    
-    II - the "centroid-pair" distance of the idealized board adn the world estimated board. Whiel
-     
-    """
-    rvec = cv2.Rodrigues(np.expand_dims(np.array([six_dof_params[0:3]], dtype=np.float32), 1))[0]
-    tvec = np.array([six_dof_params[3:]]).T
 
+    A solution to this is to fit the board to only two additional points that can be easily scaled
+    to fit precisely. This is acheived by converting all board points into two centroid values for both
+    the target board position and the estimated board position. This is represented the second part (II):
+
+    II - the "centroid-pair" distance of the idealized board and the world estimated board. Whiel
+
+    """
+    # build functioning 4x4 transformation matrix from 6 DoF parameters
+    rvec = cv2.Rodrigues(
+        np.expand_dims(np.array([six_dof_params[0:3]], dtype=np.float32), 1)
+    )[0]
+    tvec = np.array([six_dof_params[3:]]).T
     new_origin_transform = np.hstack([rvec, tvec])
     new_origin_transform = np.vstack(
         [new_origin_transform, np.array([0, 0, 0, 1], np.float32)]
@@ -156,40 +174,41 @@ def board_fit_error(six_dof_params, board_corners_xyz, world_corners_xyz ):
     xyz = world_corners_xyz
     scale = np.expand_dims(np.ones(xyz.shape[0]), 1)
     xyzh = np.hstack([xyz, scale])
-    
+
     new_origin_world_xyzh = np.matmul(np.linalg.inv(new_origin_transform), xyzh.T).T
-    new_world_corners_xyz = new_origin_world_xyzh[:,0:3]
+    new_world_corners_xyz = new_origin_world_xyzh[:, 0:3]
 
-    delta_xyz = board_corners_xyz - new_world_corners_xyz
-    delta_xyz[:2,0:2] = 0 # pin down 2 points for x,y control, otherwise ignore
-    # delta_xyz[:,2] = abs(delta_xyz[:,2]) # make the algo care more about flatness
+    new_world_corners_z = new_origin_world_xyzh[:, 2]
+    centroid_A_distance, centroid_B_distance = get_centroid_distances(
+        board_corners_xyz, new_world_corners_xyz
+    )
     
-    minimize_target = delta_xyz.ravel()
-    
-    # distance_error = np.sqrt(np.sum((board_corners_xyz - new_world_corners_xyz)**2, axis=1))
-    # if basin_hopping:
-        # distance_error = np.sum(distance_error)
-    # alternate approach here...just trying to drive the z coordinates to zero...
-    # distance_error = new_world_corners_xyz[:,2]**2
-
-    # logger.info(f"Distance Error during optimization: {distance_error}")
-    # logger.info(f"Estimated Solution: {six_dof_params}")
+    # logger.info(f"z-sum: {np.sum(new_world_corners_z)}")
+    # logger.info(f"centroid A Distances: {centroid_A_distance}")
+    # logger.info(f"centroid B Distances: {centroid_B_distance}")
+    minimize_target = np.hstack([abs(new_world_corners_z), centroid_A_distance,centroid_B_distance])
+    logger.info(f"minimize target: {minimize_target}")
     return minimize_target
 
-six_dof_params_initial = [0,0,0,0,0,0]
-# pi_plus = 4 # a longer leash than needed, but still not going crazy
-# bounds = ([-pi_plus,-pi_plus,-pi_plus, -100,-100,-10],   
-        # [pi_plus,pi_plus,pi_plus, 10,10,10])
-   
-least_sq_result = scipy.optimize.least_squares(fun = board_fit_error,
-                                            x0 = six_dof_params_initial,
-                                            # bounds=bounds,
-                                            ftol = 1e-10,
-                                            args = [board_corners_xyz,world_corners_xyz])
+
+six_dof_params_initial = [0, 0, 0, 0, 0, 0]
+pi_plus = 4 # a longer leash than needed, but still not going crazy
+bounds = ([-pi_plus,-pi_plus,-pi_plus, -100,-100,-100],
+[pi_plus,pi_plus,pi_plus, 10,10,10])
+
+least_sq_result = scipy.optimize.least_squares(
+    fun=board_fit_error,
+    x0=six_dof_params_initial,
+    bounds=bounds,
+    ftol=1e-10,
+    args=[board_corners_xyz, world_corners_xyz],
+)
 
 six_dof_params = least_sq_result.x
 
-rvec = cv2.Rodrigues(np.expand_dims(np.array(six_dof_params[0:3], dtype=np.float32), 1))[0]
+rvec = cv2.Rodrigues(
+    np.expand_dims(np.array(six_dof_params[0:3], dtype=np.float32), 1)
+)[0]
 # note that these translations result in the system moving in the negative direction
 tvec = np.array([six_dof_params[3:]]).T
 
@@ -197,7 +216,7 @@ tvec = np.array([six_dof_params[3:]]).T
 ############################## POSSIBLE SOLUTION ON PAUSE ######################
 # Commenting out code associated with attempts to use board pose....
 # attempting alternate approach of calculating R|T that minimizes the difference
-# between 
+# between
 
 # anchor_camera: CameraData = camera_array.cameras[list(camera_array.cameras.keys())[0]]
 
@@ -241,12 +260,15 @@ for port, camera_data in camera_array.cameras.items():
     new_transformation = np.dot(old_transformation, new_origin_transform)
     camera_data.transformation = new_transformation
 
-old_world_corners_xyzh = np.hstack([world_corners_xyz,np.expand_dims(np.ones(world_corners_xyz.shape[0]),1)])
-test_new_origin_world_corners_xyzh = np.matmul(np.linalg.inv(new_origin_transform), old_world_corners_xyzh.T).T
+old_world_corners_xyzh = np.hstack(
+    [world_corners_xyz, np.expand_dims(np.ones(world_corners_xyz.shape[0]), 1)]
+)
+test_new_origin_world_corners_xyzh = np.matmul(
+    np.linalg.inv(new_origin_transform), old_world_corners_xyzh.T
+).T
 
 
-
-# test_new_origin_world_corners_xyz  = 
+# test_new_origin_world_corners_xyz  =
 
 # change the point estimates to reflect the new origin
 xyz = capture_volume.point_estimates.obj
@@ -260,7 +282,9 @@ capture_volume.point_estimates.obj = new_origin_xyzh[:, 0:3]
 
 ############## REASSESS POINT ESTIMATE ORIGIN FRAME ##################################
 
-obj_indices = capture_volume.point_estimates.obj_indices[sync_indices == test_sync_index]
+obj_indices = capture_volume.point_estimates.obj_indices[
+    sync_indices == test_sync_index
+]
 # now get the actual x,y,z estimate associated with these unique charucos
 obj_xyz = capture_volume.point_estimates.obj[obj_indices]
 sorter = np.argsort(charuco_ids)

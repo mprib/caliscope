75a7d3c27c8179dd369658a15082b779d0e11e77
mprib <prible@gmail.com>
2023-03-30 12:48:44 -0500

    lots of weird little changes

--- pyxy3d/gui/stereoframe/stereo_frame_widget.py
+++ pyxy3d/gui/stereoframe/stereo_frame_widget.py
@@ -33,7 +33,7 @@ from pyxy3d.session import Session
 from pyxy3d.gui.stereoframe.stereo_frame_builder import StereoFrameBuilder
 from pyxy3d.cameras.synchronizer import Synchronizer
 from pyxy3d import __root__
-
+from pyxy3d.gui.qt_logger import QtLogger
 from pyxy3d.gui.widgets import NavigationBarBackNext
 
 class StereoFrameWidget(QWidget):
@@ -44,7 +44,7 @@ class StereoFrameWidget(QWidget):
         self.session = session
         self.synchronizer:Synchronizer = self.session.get_synchronizer()
 
-        self.frame_builder = StereoFrameBuilder(self.synchronizer, board_count_target=30)
+        self.frame_builder = StereoFrameBuilder(self.synchronizer, board_count_target=10)
         self.frame_emitter = StereoFrameEmitter(self.frame_builder)
         self.frame_emitter.start()
 
@@ -89,7 +89,7 @@ class StereoFrameWidget(QWidget):
         self.frame_emitter.ImageBroadcast.connect(self.ImageUpdateSlot)
         self.frame_rate_spin.valueChanged.connect(self.synchronizer.set_fps_target)
         self.board_count_spin.valueChanged.connect(self.update_board_count_target)
-   
+        self.frame_emitter.calibration_data_collected.connect(self.initiate_calibration)
     
     def update_board_count_target(self, target):
         self.frame_builder.board_count_target = target
@@ -104,12 +104,10 @@ class StereoFrameWidget(QWidget):
         elif self.calibrate_collect_btn.text() == "Early Terminate Collection":
             logger.info("Prematurely end data collection")
             self.frame_builder.store_points.clear()
-            self.calibrate_collect_btn.setText("Calibrate")
-            self.frame_emitter.stop()
-            self.stop_thread = Thread(target=self.session.stop_recording, args=(), daemon=True)
-        elif self.calibrate_collect_btn.text() == "Calibrate": 
-            self.session.pause_synchronizer()
             self.initiate_calibration()
+        # elif self.calibrate_collect_btn.text() == "Calibrate": 
+            # self.session.pause_synchronizer()
+            # self.initiate_calibration()
 
     def ImageUpdateSlot(self, q_image):
         self.stereo_frame_display.resize(self.stereo_frame_display.sizeHint())
@@ -118,22 +116,28 @@ class StereoFrameWidget(QWidget):
         self.stereo_frame_display.setPixmap(qpixmap)
         
         ## This is a bit of a hack and likely handled better with proper signals
-        if self.stereo_frame_display.height()==1:
-            logger.info("Target board counts acquired, ending data collection.")
-            self.calibrate_collect_btn.setText("Calibrate")
-            self.frame_emitter.stop()
-            self.stop_thread = Thread(target=self.session.stop_recording, args=(), daemon=True)
-            self.stop_thread.start()
+        # if self.stereo_frame_display.height()==1:
+            # logger.info("Target board counts acquired, ending data collection.")
+            # self.calibrate_collect_btn.setText("Calibrate")
+            # self.frame_emitter.stop()
+            # self.stop_thread = Thread(target=self.session.stop_recording, args=(), daemon=True)
+            # self.stop_thread.start()
 
 
     def initiate_calibration(self):
+        self.frame_emitter.stop()
+        self.stereo_frame_display.hide()
+        self.qt_logger = QtLogger("Initiating Calibration...")
+        self.qt_logger.show()
+        self.session.pause_synchronizer()
         self.session.stop_recording()
-        self.calibrate_thead = Thread(target=self.session.calibrate,args=(), daemon=True)
-        self.calibrate_thead.start()
+        self.calibrate_thread = Thread(target=self.session.calibrate,args=(), daemon=True)
+        self.calibrate_thread.start()
 
 class StereoFrameEmitter(QThread):
     ImageBroadcast = pyqtSignal(QImage)
-    
+    calibration_data_collected = pyqtSignal(bool) 
+
     def __init__(self, stereoframe_builder:StereoFrameBuilder):
         
         super(StereoFrameEmitter,self).__init__()
@@ -144,12 +148,17 @@ class StereoFrameEmitter(QThread):
         
     def run(self):
         while self.keep_collecting.is_set():
+            if len(self.stereoframe_builder.stereo_list) == 0:
+                self.calibration_data_collected.emit(True)
+                
             stereo_frame = self.stereoframe_builder.get_stereo_frame()
 
             if stereo_frame is not None:
                 image = cv2_to_qlabel(stereo_frame)
                 self.ImageBroadcast.emit(image)
-   
+
+            
+            
     def stop(self):
         self.keep_collecting.clear() 
 
@@ -172,7 +181,7 @@ def cv2_to_qlabel(frame):
 if __name__ == "__main__":
         App = QApplication(sys.argv)
 
-        config_path = Path(__root__, "tests", "4_cameras_beginning")
+        config_path = Path(__root__, "tests", "217")
 
         session = Session(config_path)
         session.load_cameras()

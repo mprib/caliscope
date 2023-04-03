fecbe63cc80c93a64b79b8c197c34254c1e54fb8
mprib <prible@gmail.com>
2023-04-03 13:13:45 -0500

    draft code roughly working

--- dev/link_stereopairs.py
+++ dev/link_stereopairs.py
@@ -11,51 +11,44 @@ board_counts = {(0, 1): 10,
                 (1, 2): 10, 
                 (1, 3): 0, 
                 (2, 3): 10}
-
                 
-                
-#%%
 
-missing_count_last_cycle = -1
+min_threshold = 0
+def get_empty_pairs(board_counts, min_threshold):
+    empty_pairs = [key for key, value in board_counts.items() if value ==min_threshold]
+    return empty_pairs
+
+empty_count_last_cycle = -1
 
 
-missing_stereo_pairs = 
-
-  
-while len(_get_missing_stereopairs()) != missing_count_last_cycle:
-            
+# if progress was made on gap filling last time, try again 
+# note use of walrus operator (:=). Not typically used  but it works here
+while len(empty_pairs := get_empty_pairs(board_counts, min_threshold)) != empty_count_last_cycle:
+         
     # prep the variable. if it doesn't go down, terminate
-    missing_count_last_cycle = len(_get_missing_stereopairs())
+    empty_count_last_cycle = len(empty_pairs)
 
-    for pair in _get_missing_stereopairs():
+    for pair in empty_pairs:
              
         port_A = pair[0]
         port_C = pair[1]
-    
-        # get lists of all the estimiated stereopairs that might bridge across test_missing
-        all_pairs_A_X = [pair for pair in estimated_stereopairs.keys() if pair[0]==port_A]
-        all_pairs_X_C = [pair for pair in estimated_stereopairs.keys() if pair[1]==port_C]
+
+        all_pairs_A_X = [pair for pair in board_counts.keys() if pair[0]==port_A]
+        all_pairs_X_C = [pair for pair in board_counts.keys() if pair[1]==port_C]
    
-        stereopair_A_C = None
+        board_count_A_C = None
 
         for pair_A_X in all_pairs_A_X:
             for pair_X_C in all_pairs_X_C:
                 if pair_A_X[1] == pair_X_C[0]:
                     # A bridge can be formed!
-                    stereopair_A_X = estimated_stereopairs[pair_A_X]
-                    stereopair_X_C = estimated_stereopairs[pair_X_C]
-                    possible_stereopair_A_C = get_bridged_stereopair(stereopair_A_X, stereopair_X_C)
-                    if stereopair_A_C is None:
-                        # current possibility is better than nothing
-                        stereopair_A_C = possible_stereopair_A_C
-                    else:
-                        # check if it's better than what you have already
-                        # if it is, then overwrite the old one
-                        if stereopair_A_C.error_score > possible_stereopair_A_C.error_score:
-                            stereopair_A_C = possible_stereopair_A_C
+                    board_count_A_X = board_counts[pair_A_X]
+                    board_count_X_C = board_counts[pair_X_C]
+                    
+                    if board_count_A_X > min_threshold and board_count_X_C > min_threshold:
+                        board_count_A_C = min(board_count_A_X,board_count_X_C) 
+                        board_counts[pair] = board_count_A_C
+            
+board_counts
 
-        if stereopair_A_C is not None:
-            add_stereopair(stereopair_A_C)
-
-if len(_get_missing_stereopairs()) > 0:
-    raise ValueError("Insufficient stereopairs to allow array to be estimated")
+# %%

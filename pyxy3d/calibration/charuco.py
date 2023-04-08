15bcdd3a6a87966a8bac9dedccfb8ec61f137dec
mprib <prible@gmail.com>
2023-04-08 10:53:02 -0500

    move get_charuco to configurator

--- pyxy3d/calibration/charuco.py
+++ pyxy3d/calibration/charuco.py
@@ -221,29 +221,6 @@ ARUCO_DICTIONARIES = {
     "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11,
 }
 
-def get_charuco(config_path)-> Charuco:
-    """
-    Helper function to load a pre-configured charuco from a config.toml
-    """
-
-    with open(config_path, "r") as f:
-        config = toml.load(config_path)
-
-    params = config["charuco"]
-
-    charuco = Charuco(
-        columns=params["columns"],
-        rows=params["rows"],
-        board_height=params["board_height"],
-        board_width=params["board_width"],
-        dictionary=params["dictionary"],
-        units=params["units"],
-        aruco_scale=params["aruco_scale"],
-        square_size_overide_cm=params["square_size_overide_cm"],
-        inverted=params["inverted"],
-    )
-    
-    return charuco
 
 ########################## DEMO  ###########################################
 

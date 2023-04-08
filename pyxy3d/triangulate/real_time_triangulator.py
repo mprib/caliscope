22facb9ec1eba1c701529c6daa3089bfcade2957
mprib <prible@gmail.com>
2023-04-08 10:13:16 -0500

    drafting RTT, but need configurator...

--- pyxy3d/triangulate/real_time_triangulator.py
+++ pyxy3d/triangulate/real_time_triangulator.py
@@ -1,7 +1,9 @@
 from pyxy3d.cameras.camera_array import CameraArray
+from pyxy3d.cameras.synchronizer import Synchronizer
 
 
 class RealTimeTriangulator:
     
-    def __init__(self,camera_array:CameraArray):
-        self.camera_array = CameraArray
+    def __init__(self,camera_array:CameraArray, synchronizer:Synchronizer):
+        self.camera_array = camera_array
+        self.synchronizer = synchronizer

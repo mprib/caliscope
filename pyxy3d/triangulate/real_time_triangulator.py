2ac9fdc79b99d45c8a9273fee0724c302fa45b51
mprib <prible@gmail.com>
2023-04-08 13:07:21 -0500

    real time triangulator pulling in sync packets

--- pyxy3d/triangulate/real_time_triangulator.py
+++ pyxy3d/triangulate/real_time_triangulator.py
@@ -1,9 +1,37 @@
-from pyxy3d.cameras.camera_array import CameraArray
-from pyxy3d.cameras.synchronizer import Synchronizer
+import pyxy3d.logger
+logger = pyxy3d.logger.get(__name__)
 
+from pyxy3d.cameras.camera_array import CameraArray
+from pyxy3d.cameras.synchronizer import Synchronizer, SyncPacket
+from queue import Queue
+from threading import Thread, Event
 
 class RealTimeTriangulator:
     
     def __init__(self,camera_array:CameraArray, synchronizer:Synchronizer):
         self.camera_array = camera_array
         self.synchronizer = synchronizer
+        
+        self.stop_thread = Event()
+        self.stop_thread.clear()
+        
+        self.sync_packet_in_q = Queue(-1) 
+        self.synchronizer.subscribe_to_sync_packets(self.sync_packet_in_q)
+
+        self.thread = Thread(target=self.process_incoming, args=(), daemon=True)
+        self.thread.start()
+        self.running = True
+
+    def process_incoming(self):
+        
+        while not self.stop_thread.is_set():
+
+            sync_packet:SyncPacket = self.sync_packet_in_q.get()
+            if sync_packet is None:
+                # No more sync packets after this... wind down
+                self.stop_thread.set()
+                
+            logger.info("Sync Packet Grabbed...")     
+
+        
+        self.running = False        

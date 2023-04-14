de5b7539d0ff0aba2bd3760b43038cce67ec518c
mprib <31831778+mprib@users.noreply.github.com>
2023-04-14 13:30:44 -0500

    format

--- pyxy3d/interface.py
+++ pyxy3d/interface.py
@@ -3,7 +3,7 @@ import numpy as np
 from numba.typed import List
 from queue import Queue
 from abc import ABC, abstractmethod
-import cv2    
+import cv2
 
 
 @dataclass
@@ -13,19 +13,25 @@ class PointPacket:
     A calleable that receives an image frame and returns a point_packet
     """
 
-    point_id: np.ndarray = None # unique point id that aligns with Tracker.get_point_names()
-    img_loc: np.ndarray = None # x,y position of tracked point
-    obj_loc: np.ndarray = None # x,y,z in object frame of reference; primarily for calibration
-    confidence: np.ndarray = None # may be available in some trackers..include for downstream 
+    point_id: np.ndarray = (
+        None  # unique point id that aligns with Tracker.get_point_names()
+    )
+    img_loc: np.ndarray = None  # x,y position of tracked point
+    obj_loc: np.ndarray = (
+        None  # x,y,z in object frame of reference; primarily for calibration
+    )
+    confidence: np.ndarray = (
+        None  # may be available in some trackers..include for downstream
+    )
+
 
 class Tracker(ABC):
-    
     @abstractmethod
-    def get_points(self, frame:np.ndarray)->PointPacket:
+    def get_points(self, frame: np.ndarray) -> PointPacket:
         pass
 
     @abstractmethod
-    def get_point_names(self)->dict:
+    def get_point_names(self) -> dict:
         """
         Used for saving out data with sensible headers
         """
@@ -35,26 +41,28 @@ class Tracker(ABC):
         """
         used for drawing purposes elsewhere. Specify which
         points (if any) should have a line connecting them
-        
+
         """
         pass
+
+
 class Stream(ABC):
     """
     As much an exercise in better understanding ABC as it is anything...
     """
+
     @abstractmethod
-    def subscribe(self,queue:Queue):
-        pass
-    
-    @abstractmethod
-    def unsubscribe(self,queue:Queue):
+    def subscribe(self, queue: Queue):
         pass
 
     @abstractmethod
-    def set_tracking_on(self,track:bool):
+    def unsubscribe(self, queue: Queue):
         pass
 
-    
+    @abstractmethod
+    def set_tracking_on(self, track: bool):
+        pass
+
     @abstractmethod
     def process_frames(self):
         pass
@@ -75,30 +83,29 @@ class FramePacket:
 
     def to_tidy_table(self, sync_index):
         """
-        Returns a dictionary of lists where each list is as long as the 
+        Returns a dictionary of lists where each list is as long as the
         number of points identified on the frame
         """
-        
+
         point_count = len(self.points.point_id)
         if point_count > 0:
             table = {
-                    "sync_index": [sync_index]*point_count,
-                    "port":[self.port]*point_count,
-                    "frame_index":[self.frame_index]*point_count,
-                    "frame_time":[self.frame_time]*point_count,
-                    "point_id":self.points.point_id.tolist(),
-                    "img_loc_x":self.points.img_loc[:,0].tolist(),
-                    "img_loc_y": self.points.img_loc[:,1].tolist(),
-                    "obj_loc_x":self.points.obj_loc[:,0].tolist(),
-                    "obj_loc_y":self.points.obj_loc[:,1].tolist()
-                    }       
+                "sync_index": [sync_index] * point_count,
+                "port": [self.port] * point_count,
+                "frame_index": [self.frame_index] * point_count,
+                "frame_time": [self.frame_time] * point_count,
+                "point_id": self.points.point_id.tolist(),
+                "img_loc_x": self.points.img_loc[:, 0].tolist(),
+                "img_loc_y": self.points.img_loc[:, 1].tolist(),
+                "obj_loc_x": self.points.obj_loc[:, 0].tolist(),
+                "obj_loc_y": self.points.obj_loc[:, 1].tolist(),
+            }
         else:
-            table = None 
+            table = None
         return table
 
     @property
     def frame_with_points(self):
-        
         if self.points is not None:
             drawn_frame = self.frame.copy()
             locs = self.points.img_loc
@@ -109,10 +116,10 @@ class FramePacket:
                 cv2.circle(drawn_frame, (x, y), 5, (0, 0, 220), 3)
         else:
             drawn_frame = self.frame.copy()
-            
+
         return drawn_frame
-            
-        
+
+
 @dataclass
 class SyncPacket:
     """
@@ -122,27 +129,25 @@ class SyncPacket:
     sync_index: int
     frame_packets: dict
 
-
     @property
     def triangulation_inputs(self):
         """
-        returns three key items used by the triangulation functions 
+        returns three key items used by the triangulation functions
             cameras: a list of the camera ids associated with each reported 2d point
             point_ids: the point id associated with each 2d point
             img_xy: the 2d image points themselves
-        
+
         """
         cameras = []
         point_ids = []
         img_xy = []
 
         for port, packet in self.frame_packets.items():
-            cameras.extend([port]*len(packet.points.point_id))
-            point_ids.extend(packet.points.point_id.tolist())            
+            cameras.extend([port] * len(packet.points.point_id))
+            point_ids.extend(packet.points.point_id.tolist())
             img_xy.extend(packet.points.img_loc.tolist())
-        
-        return cameras, point_ids,img_xy
 
+        return cameras, point_ids, img_xy
 
     @property
     def dropped(self):
@@ -150,18 +155,16 @@ class SyncPacket:
         convencience method to ease tracking of dropped frame rate within the synchronizer
         """
         temp_dict = {}
-        for port,packet in self.frame_packets.items():
+        for port, packet in self.frame_packets.items():
             if packet is None:
                 temp_dict[port] = 1
             else:
                 temp_dict[port] = 0
         return temp_dict
 
- 
+
 @dataclass
 class XYZPacket:
-    sync_index:int
-    point_ids:List
-    point_xyz:List
-    
-        
+    sync_index: int
+    point_ids: List
+    point_xyz: List

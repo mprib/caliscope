33ddbc7df6664e7d3969180081b584d31db00ba6
mprib <prible@gmail.com>
2023-04-30 12:00:48 -0500

    scope out changes

---
+++ dev/dev_configurator.py
@@ -0,0 +1,46 @@
+# A place for me to sort out the details of this current refactor.
+# I want to be able to construct all needed objects from a config file,
+# as well as save out modified objects to that config file.
+
+# This will turn into a test of the configurator... Not really looking to 
+# perform the calibration. Just load everything, then save everything out, 
+# to a different file and make sure stuff is the same.
+
+
+
+# initialize
+# provided with a path, load toml or create a default toml.
+
+# load cameras
+
+
+
+# load streams *from cameras* (these do not require loading from config)
+
+
+
+# load charuco
+
+
+# save cameras
+
+
+
+# load charuco
+
+
+
+# save charuco
+
+
+# load camera array (composed of  camera data objects for calibration/triangulation)
+
+
+
+
+
+
+
+
+
+

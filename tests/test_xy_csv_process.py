d2d6b01ab224ae5e2618331ad2d69220b9f40e64
mprib <31831778+mprib@users.noreply.github.com>
2023-05-05 15:47:22 -0500

    format

--- tests/test_xy_csv_process.py
+++ tests/test_xy_csv_process.py
@@ -15,7 +15,7 @@ filtering of the data using reprojection error...
 
 """
 
-#%%
+# %%
 import pyxy3d.logger
 
 logger = pyxy3d.logger.get(__name__)
@@ -33,13 +33,13 @@ from pyxy3d.post_processing_pipelines import create_xy_points
 
 
 def test_xy_point_creation():
-    # create a clean directory to start from 
+    # create a clean directory to start from
     session_path = Path(__root__, "tests", "sessions", "mediapipe_calibration_2_cam")
     copy_session_path = Path(
         __root__, "tests", "sessions_copy_delete", "mediapipe_calibration_2_cam"
     )
     copy_contents(session_path, copy_session_path)
-    
+
     # create inputs to processing pipeline function
     config = Configurator(copy_session_path)
     tracker_factory = HandTrackerFactory()
@@ -50,28 +50,29 @@ def test_xy_point_creation():
 
     create_xy_points(config, recording_directory, tracker_factory)
 
-
     # make some basic assertions against the created files
     produced_files = [
         Path(recording_directory, "xy.csv"),
         Path(recording_directory, "port_0_xy.mp4"),
         Path(recording_directory, "port_1_xy.mp4"),
     ]
+
     for file in produced_files:
         logger.info(f"Asserting that the following file exists: {file}")
         assert file.exists()
 
     # confirm that xy data is produced for the sync indices (slightly reduced to avoid missing data issues)
     xy_data = pd.read_csv(Path(recording_directory, "xy.csv"))
-    xy_sync_index_count = xy_data["sync_index"].max() + 1 # zero indexed
+    xy_sync_index_count = xy_data["sync_index"].max() + 1  # zero indexed
 
-    logger.info(f"Sync index count in frame history: {sync_index_count} in frame history")
+    logger.info(
+        f"Sync index count in frame history: {sync_index_count} in frame history"
+    )
     logger.info(f"Max sync index: {xy_data['sync_index'].max()} in xy.csv")
 
-    LEEWAY = 2 # sync indices that might not get copied over due to not enough frames
-    assert(sync_index_count-LEEWAY <= xy_sync_index_count)
+    LEEWAY = 2  # sync indices that might not get copied over due to not enough frames
+    assert sync_index_count - LEEWAY <= xy_sync_index_count
 
 
 if __name__ == "__main__":
-    
     test_xy_point_creation()

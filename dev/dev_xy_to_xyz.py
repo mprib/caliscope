403c8a68663fee50b8ae39158ae925e94ab923d4
mprib <prible@gmail.com>
2023-05-07 09:34:29 -0500

    don't create points from scratch

--- dev/dev_xy_to_xyz.py
+++ dev/dev_xy_to_xyz.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 from pyxy3d.helper import copy_contents
 from pyxy3d import __root__
+from pyxy3d.triangulate.sync_packet_triangulator import triangulate_sync_index
 from pyxy3d.configurator import Configurator
 from pyxy3d.post_processing_pipelines import create_xy_points
 from pyxy3d.trackers.hand_tracker import HandTrackerFactory
@@ -25,16 +26,16 @@ working_data = Path(__root__, "tests", "sessions_copy_delete", "4_cam_recording"
 copy_contents(origin_data, working_data)
 
 config = Configurator(working_data)
-recording_directory = Path(working_data, "recording_1")
+recording_directory = Path(working_data, "recording_1_frames_processed")
 xy_path = Path(recording_directory, "xy.csv")
 
 
 # need to initially create the xy data....
-create_xy_points(
-    config=config,
-    recording_directory=recording_directory,
-    tracker_factory=HandTrackerFactory(),
-)
+# create_xy_points(
+#     config=config,
+#     recording_directory=recording_directory,
+#     tracker_factory=HandTrackerFactory(),
+# )
 
 xy_data = pd.read_csv(xy_path)
 

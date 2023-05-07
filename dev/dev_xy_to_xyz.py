daa178d8fb4abf1db1206b88b50a0d5ebf1a36c6
mprib <prible@gmail.com>
2023-05-07 13:48:02 -0500

    clean up unused imports

--- dev/dev_xy_to_xyz.py
+++ dev/dev_xy_to_xyz.py
@@ -1,100 +1,54 @@
-"""
-Building test regarding the conversion of the xy.csv datafile into an xyz.csv datafile.
-I suppose that it may make sense to run this through the same processing pipeline 
-as the SyncPacketTriangulator...no sense reinventing the wheel.
-
-
-
-"""
-
-# %%
-
 import pyxy3d.logger
 
 logger = pyxy3d.logger.get(__name__)
 import time
 import pandas as pd
 from pathlib import Path
-from numba.typed import Dict, List
-import numpy as np
-
-from pyxy3d.helper import copy_contents
 from pyxy3d import __root__
-from pyxy3d.triangulate.sync_packet_triangulator import triangulate_sync_index
 from pyxy3d.configurator import Configurator
-from pyxy3d.cameras.camera_array import CameraArray
-from pyxy3d.post_processing_pipelines import create_xy_points
-from pyxy3d.trackers.hand_tracker import HandTrackerFactory
+from pyxy3d.post_processing_pipelines import triangulate_xy_data
 
-# load in file of xy point data
-origin_data = Path(__root__, "tests", "sessions", "4_cam_recording")
-working_data = Path(__root__, "tests", "sessions_copy_delete", "4_cam_recording")
+def test_xy_to_xyz_postprocessing():
+    # load in file of xy point data
+    origin_data = Path(__root__, "tests", "sessions", "4_cam_recording")
 
-copy_contents(origin_data, working_data)
+    config = Configurator(origin_data)
+    recording_directory = Path(origin_data, "recording_1_frames_processed")
+    xy_path = Path(recording_directory, "xy.csv")
 
-config = Configurator(working_data)
-recording_directory = Path(working_data, "recording_1_frames_processed")
-xy_path = Path(recording_directory, "xy.csv")
+    xy_data = pd.read_csv(xy_path)
+    camera_array = config.get_camera_array()
 
+    start = time.time()
+    logger.info(f"beginning triangulation at {time.time()}")
+    xyz_history = triangulate_xy_data(xy_data, camera_array)
+    logger.info(f"ending triangulation at {time.time()}")
+    stop = time.time()
+    logger.info(f"Elapsed time is {stop-start}. Note that on first iteration, @jit functions will take longer")
 
-# need to initially create the xy data....
-# create_xy_points(
-#     config=config,
-#     recording_directory=recording_directory,
-#     tracker_factory=HandTrackerFactory(),
-# )
+    # Assert that the xyz_history dictionary has the expected keys
+    assert set(xyz_history.keys()) == {"point_id", "x_coord", "y_coord", "z_coord"}
 
-xy_data = pd.read_csv(xy_path)
-camera_array = config.get_camera_array()
+    # Assert that all lists in xyz_history have the same length
+    assert (
+        len(xyz_history["point_id"])
+        == len(xyz_history["x_coord"])
+        == len(xyz_history["y_coord"])
+        == len(xyz_history["z_coord"])
+    )
 
-
-def triangulate_xy_data(xy_data:pd.DataFrame, camera_array:CameraArray)->Dict[str,List]:
-
-    # assemble numba compatible dictionary
-    projection_matrices = Dict()
-    for port, cam in camera_array.cameras.items():
-        projection_matrices[int(port)] = cam.projection_matrix
-    
-    xyz_history = {"point_id":[],
-                   "x_coord": [],
-                   "y_coord": [],
-                   "z_coord": [],}
-    
-    for index in xy_data["sync_index"].unique():
-        
-        active_index = xy_data["sync_index"] == index
-        cameras = xy_data["port"][active_index].to_numpy()
-        point_ids = xy_data["point_id"][active_index].to_numpy()
-        img_loc_x = xy_data["img_loc_x"][active_index].to_numpy()
-        img_loc_y = xy_data["img_loc_y"][active_index].to_numpy()
-        imgs_xy = np.vstack([img_loc_x, img_loc_y]).T
-
-        point_id_xyz, points_xyz = triangulate_sync_index(
-            projection_matrices, cameras, point_ids, imgs_xy
-        )
-
-        if len(point_id_xyz) > 0:        
-            # there are points to store so store them...
-            points_xyz = np.array(points_xyz)
-            xyz_history["point_id"].extend(point_id_xyz)
-            xyz_history["x_coord"].extend(points_xyz[:,0].tolist())
-            xyz_history["y_coord"].extend(points_xyz[:,1].tolist())
-            xyz_history["z_coord"].extend(points_xyz[:,2].tolist())
-
-    return xyz_history
-
-start = time.time()
-logger.info(f"beginning triangulation at {time.time()}")
-xyz_history = triangulate_xy_data(xy_data, camera_array)
-logger.info(f"ending triangulation at {time.time()}")
-stop = time.time()
-logger.info(f"Elapsed time is {stop-start}. Note that on first iteration, @jit functions will take longer")
-
-# run some basic assertions against the triangulated output
+    # Assert that coordinates are within expected bounds aroundt the set origin
+    min_x, max_x = -1, 1    
+    min_y, max_y = -1, 1    
+    min_z, max_z = -2, 4    
+    for x, y, z in zip(xyz_history["x_coord"], xyz_history["y_coord"], xyz_history["z_coord"]):
+        assert min_x <= x <= max_x
+        assert min_y <= y <= max_y
+        assert min_z <= z <= max_z
 
 
 
+    output_path = Path(recording_directory, "xyz.csv")
+    xyz_history = pd.DataFrame(xyz_history)
+    xyz_history.to_csv(output_path)
 
-output_path = Path(recording_directory, "xyz.csv")
-xyz_history = pd.DataFrame(xyz_history)
-xyz_history.to_csv(output_path)

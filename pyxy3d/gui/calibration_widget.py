c0a2059d2ab1d7ba1a8d34bee596bd7c83ded872
mprib <31831778+mprib@users.noreply.github.com>
2023-05-31 12:14:05 -0500

    integrate new session modes into widget

--- pyxy3d/gui/calibration_widget.py
+++ pyxy3d/gui/calibration_widget.py
@@ -69,120 +69,56 @@ class CalibrationWidget(QStackedWidget):
         pass
 
 
-    ######################## STEP 1: Charuco Builder ###########################
 
     def activate_camera_config(self):
-        if hasattr(self, "camera_config"):
-            logger.info("Camera config already exists; changing stack current index")
-            self.session.set_mode(SessionMode.IntrinsicCalibration)
-            active_port = self.camera_config.camera_tabs.currentIndex()
-            self.session.active_monocalibrator = active_port
-            self.setCurrentWidget(self.camera_config)
-            # self.camera_config.camera_tabs.toggle_tracking(active_port)
-
-            # charuco board might have changed, so...
-        else:
-            logger.info("Initiating Camera Connection")
-            self.initiate_camera_connection()
-
-    def initiate_camera_connection(self):
-        if len(self.session.streams) > 0:
-            logger.info("Cameras already connected")
-        else:
-
-            def connect_to_cams_worker():
-                self.CAMS_IN_PROCESS = True
-                logger.info("Initiating camera connect worker")
-
-                # find out if you are loading cameras or finding cameras
-                if self.session.get_configured_camera_count() > 0:
-                    # self.# session.load_cameras()
-                    try:
-                        pass
-
-                    except:
-                        logger.warn("it wasn't safe to comment out this code")
-                        #Mac, if you come back to this well past branch 346 and don't know
-                        # what this is about, just delete it all including the get_configured_count method
-                    
-                    # logger.info("Camera connect worker about to load stream tools")
-
-                    # self.session.load_streams(
-                    #     tracker=CharucoTracker(self.session.charuco)
-                    # )
-                else:
-                    logger.info(
-                        f"No previous configured cameras detected...searching for cameras...."
-                    )
-                    self.session._find_cameras()
-                # logger.info("Camera connect worker about to adjust resolutions")
-                # self.session._adjust_resolutions()
-                # logger.info("Camera connect worker about to load monocalibrators")
-                # self.session._load_monocalibrators()
-                self.session.load_stream_tools()
-                self.CAMS_IN_PROCESS = False
-
-                logger.info("emitting cameras_connected signal")
-                self.cameras_connected.emit()
-                self.camera_config = CameraWizard(self.session)
-                self.addWidget(self.camera_config)
-                self.setCurrentWidget(self.camera_config)
-                self.camera_config.navigation_bar.back_btn.clicked.connect(
-                    self.back_to_charuco_wizard
-                )
-                self.camera_config.navigation_bar.next_btn.clicked.connect(
-                    self.next_to_stereoframe
-                )
-
-                # if hasattr(self, "qt_logger"):
-                #     del self.qt_logger
-                # self.qt_logger.hide()
-
-        if self.CAMS_IN_PROCESS:
-            logger.info("Already attempting to connect to cameras...")
-        else:
-            self.connect_cams = Thread(
-                target=connect_to_cams_worker, args=[], daemon=True
+        self.session.set_mode(SessionMode.IntrinsicCalibration)
+        if not hasattr(self, "camera_wizard"):
+            logger.info(f"No camera configuration yet...creating wizard")
+            self.camera_wizard = CameraWizard(self.session)
+            self.addWidget(self.camera_wizard)
+            self.setCurrentWidget(self.camera_wizard)
+            self.camera_wizard.navigation_bar.back_btn.clicked.connect(
+                self.activate_charuco_wizard
             )
-            self.connect_cams.start()
-
-    # def on_cameras_connect(self):
-        # load cameras config once the cameras are actually connected
+            self.camera_wizard.navigation_bar.next_btn.clicked.connect(
+                self.next_to_stereoframe
+            )
+        else:
+            logger.info("Camera config already exists; changing stack current index")
+            # active_port = self.camera_wizard.camera_tabs.currentIndex()
+            # self.session.active_monocalibrator = active_port
+            self.setCurrentWidget(self.camera_wizard)
+            self.session.activate_monocalibrator()
 
     ####################### STEP 2: Single Camera Calibration #################
-    def back_to_charuco_wizard(self):
+    def activate_charuco_wizard(self):
+        self.session.set_mode(SessionMode.Charuco)
         self.setCurrentWidget(self.wizard_charuco)
-        self.session.pause_all_monocalibrators()
 
     def next_to_stereoframe(self):
-        self.session.pause_all_monocalibrators()
+        self.session.set_mode(SessionMode.ExtrinsicCalibration)
 
-        if hasattr(self.session, "synchronizer"):
-            self.session.unpause_synchronizer()
-
-        self.launch_new_stereoframe()
-
-    def launch_new_stereoframe(self):
         if hasattr(self, "stereoframe"):
-            self.stereoframe.deleteLater()
-            self.removeWidget(self.stereoframe)
-            
-        self.stereoframe = StereoFrameWidget(self.session)
-        self.addWidget(self.stereoframe)
-        self.setCurrentWidget(self.stereoframe)
+            self.setCurrentWidget(self.stereoframe)
+            # self.stereoframe.deleteLater()
+            # self.removeWidget(self.stereoframe)
+        else: 
+            self.stereoframe = StereoFrameWidget(self.session)
+            self.addWidget(self.stereoframe)
+            self.setCurrentWidget(self.stereoframe)
 
-        self.stereoframe.navigation_bar.back_btn.clicked.connect(
-            self.back_to_camera_config_wizard
-        )
+            self.stereoframe.navigation_bar.back_btn.clicked.connect(
+                self.back_to_camera_config_wizard
+            )
 
-        self.stereoframe.calibration_complete.connect(self.next_to_capture_volume)
-        # self.stereoframe.calibration_initiated.connect(self.show_calibration_qt_logger)
-        self.stereoframe.terminate.connect(self.refresh_stereoframe)
+            self.stereoframe.calibration_complete.connect(self.next_to_capture_volume)
+            # self.stereoframe.calibration_initiated.connect(self.show_calibration_qt_logger)
+            self.stereoframe.terminate.connect(self.refresh_stereoframe)
 
     ###################### Stereocalibration  ######################################
     def refresh_stereoframe(self):
         logger.info("Set current widget to config temporarily")
-        self.setCurrentWidget(self.camera_config)
+        self.setCurrentWidget(self.camera_wizard)
 
         logger.info("Remove stereoframe")
         self.removeWidget(self.stereoframe)
@@ -192,26 +128,18 @@ class CalibrationWidget(QStackedWidget):
         logger.info("Create new stereoframe")
         self.launch_new_stereoframe()
 
-    # def show_calibration_qt_logger(self):
-    #     """
-    #     Calibration is initiated back on the stereoframe widget,here only
-    #     the logger launch is managed because it is main that must delete the logger
-    #     """
-    #     logger.info("Launching calibration qt logger")
-    #     self.qt_logger = QtLogger("Calibrating camera array...")
-    #     self.qt_logger.show()
 
     def back_to_camera_config_wizard(self):
         logger.info("Moving back to camera config from stereoframe")
-        self.setCurrentWidget(self.camera_config)
+        self.setCurrentWidget(self.camera_wizard)
         self.session.pause_synchronizer()
 
         self.stereoframe.frame_builder.unsubscribe_from_synchronizer()
         self.removeWidget(self.stereoframe)
         del self.stereoframe
 
-        active_port = self.camera_config.camera_tabs.currentIndex()
-        self.camera_config.camera_tabs.toggle_tracking(active_port)
+        active_port = self.camera_wizard.camera_tabs.currentIndex()
+        self.camera_wizard.camera_tabs.activate_current_tab(active_port)
 
     def next_to_capture_volume(self):
         logger.info("Creating Capture Volume widget")
@@ -229,7 +157,7 @@ class CalibrationWidget(QStackedWidget):
     ################## Capture Volume ########################
     def back_to_stereo_frame(self):
         logger.info("Set current widget to config temporarily")
-        self.setCurrentWidget(self.camera_config)
+        self.setCurrentWidget(self.camera_wizard)
 
         logger.info("Remove stereoframe")
         self.removeWidget(self.stereoframe)

ee7b3a04cf93a646f790f535098cb0b04109a478
mprib <prible@gmail.com>
2023-06-01 15:38:48 -0500

    granular demo of extrinsic calibration

---
+++ dev/demo/demo_gui_extrinsic_calibration.py
@@ -0,0 +1,26 @@
+from PyQt6.QtWidgets import QApplication
+import sys
+from pyxy3d import __root__
+from pathlib import Path
+import toml
+from pyxy3d import __app_dir__
+from pyxy3d.gui.main_widget import MainWindow
+from pyxy3d.configurator import Configurator
+from pyxy3d.gui.extrinsic_calibration_widget import ExtrinsicCalibrationWidget
+from pyxy3d.session.session import Session, SessionMode
+
+
+app_settings = toml.load(Path(__app_dir__, "settings.toml"))
+recent_projects: list = app_settings["recent_projects"]
+
+recent_project_count = len(recent_projects)
+session_path = Path(recent_projects[recent_project_count - 1])
+config = Configurator(session_path)
+session = Session(config)
+session.set_mode(SessionMode.ExtrinsicCalibration)
+
+app = QApplication(sys.argv)
+window = ExtrinsicCalibrationWidget(session)
+window.show()
+
+app.exec()

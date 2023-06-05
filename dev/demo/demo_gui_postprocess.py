68c5914e815d7fcbc7c0231fbfe5cf61311f2e5c
mprib <prible@gmail.com>
2023-06-05 11:42:54 -0500

    explicit selection of session

--- dev/demo/demo_gui_postprocess.py
+++ dev/demo/demo_gui_postprocess.py
@@ -10,6 +10,7 @@ import toml
 from time import sleep
 from pyxy3d import __root__
 from pyxy3d.configurator import Configurator
+from pyxy3d.session.session import Session
 from pathlib import Path
 from pyxy3d import __app_dir__
 
@@ -18,13 +19,17 @@ app_settings = toml.load(Path(__app_dir__, "settings.toml"))
 recent_projects:list = app_settings["recent_projects"]
 
 recent_project_count = len(recent_projects)
-session_path = Path(recent_projects[recent_project_count-1])
+# session_path = Path(recent_projects[recent_project_count-1])
+session_path = Path(app_settings["last_project_parent"], "keng_hung")
+
 logger.info(f"Launching post processing widget for {session_path}")
 
 config =  Configurator(session_path)
+session = Session(config)
 
 app = QApplication(sys.argv)
-window = PostProcessingWidget(config)
+
+window = PostProcessingWidget(session)
 
 # open in a session already so you don't have to go through the menu each time
 # window.open_session(config_path)

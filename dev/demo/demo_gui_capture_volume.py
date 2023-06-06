8dd299ad9dd380f5020877f7458cfc783d4742df
mprib <prible@gmail.com>
2023-06-06 13:51:10 -0500

    demo to test single widget for simplicity

---
+++ dev/demo/demo_gui_capture_volume.py
@@ -0,0 +1,28 @@
+from PyQt6.QtWidgets import QApplication
+from pyxy3d.gui.calibrate_capture_volume_widget import CalibrateCaptureVolumeWidget
+from pyxy3d.gui.vizualize.calibration.capture_volume_widget import CaptureVolumeWidget
+import sys
+from time import sleep
+from pyxy3d import __root__
+from pathlib import Path
+from pyxy3d.configurator import Configurator
+from pyxy3d.session.session import Session, SessionMode
+import toml
+from pyxy3d import __app_dir__
+
+app_settings = toml.load(Path(__app_dir__, "settings.toml"))
+recent_projects:list = app_settings["recent_projects"]
+
+recent_project_count = len(recent_projects)
+session_path = Path(recent_projects[recent_project_count-1])
+
+config = Configurator(session_path)
+session = Session(config)
+session.load_estimated_capture_volume()
+# session.set_mode(SessionMode.ExtrinsicCalibration)
+app = QApplication(sys.argv)
+window = CaptureVolumeWidget(session)
+
+window.show()
+
+app.exec()

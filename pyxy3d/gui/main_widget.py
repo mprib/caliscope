10d195bbe27dbacb92be8cb01d48c3ad6b85e660
mprib <prible@gmail.com>
2023-06-06 18:50:18 -0500

    don't bother with keyboard shortcuts yet

--- pyxy3d/gui/main_widget.py
+++ pyxy3d/gui/main_widget.py
@@ -1,4 +1,5 @@
 import pyxy3d.logger
+import pyxy3d.logger
 
 logger = pyxy3d.logger.get(__name__)
 
@@ -47,15 +48,15 @@ class MainWindow(QMainWindow):
 
         # File Menu
         self.menu = self.menuBar()
-        self.file_menu = self.menu.addMenu("&File")
+        self.file_menu = self.menu.addMenu("File")
 
         # Open or New project (can just create a folder in the dialog in truly new)
-        self.open_project_action = QAction("&New/Open Project", self)
+        self.open_project_action = QAction("New/Open Project", self)
         self.open_project_action.triggered.connect(self.create_new_project_folder)
         self.file_menu.addAction(self.open_project_action)
 
         # Open Recent
-        self.open_recent_project_submenu = QMenu("&Recent Projects...", self)
+        self.open_recent_project_submenu = QMenu("Recent Projects...", self)
         # Populate the submenu with recent project paths;
         # reverse so that last one appended is at the top of the list
         for project_path in reversed(self.app_settings["recent_projects"]):
@@ -63,13 +64,13 @@ class MainWindow(QMainWindow):
 
         self.file_menu.addMenu(self.open_recent_project_submenu)
 
-        self.close_session_action = QAction("&Close Session", self)
-        self.close_session_action.triggered.connect(self.close_current_session)
+        self.close_session_action = QAction("Close Session", self)
+        # self.close_session_action.triggered.connect(self.close_current_session)
         self.file_menu.addAction(self.close_session_action)
 
-        self.cameras_menu = self.menu.addMenu("Ca&meras")
-        self.disconnect_cameras_action = QAction("&Disconnect Cameras", self)
-        self.connect_cameras_action = QAction("Co&nnect Cameras", self)
+        self.cameras_menu = self.menu.addMenu("Cameras")
+        self.disconnect_cameras_action = QAction("Disconnect Cameras", self)
+        self.connect_cameras_action = QAction("Connect Cameras", self)
         self.cameras_menu.addAction(self.disconnect_cameras_action)
         self.cameras_menu.addAction(self.connect_cameras_action)
 
@@ -97,52 +98,39 @@ class MainWindow(QMainWindow):
         logger.info(f"Switching main window to tab {index}")
         match index:
             case 0:
-                logger.info(f"Activate Calibration Mode")
-                if hasattr(self.calibration_widget, "currentWidget"):
-                    match self.calibration_widget.currentWidget():
-                        case self.calibration_widget.intrinsic_calibration_widget:
-                            active_camera = (
-                                self.calibration_widget.intrinsic_calibration_widget.camera_tabs.currentWidget().port
-                            )
-                            logger.info(
-                                f"Activating intrinsic calibration tab: camera config widget with Camera {active_camera} active"
-                            )
-                            self.session.set_mode(SessionMode.IntrinsicCalibration)
-                        case self.calibration_widget.extrinsic_calibration_widget:
-                            logger.info(
-                                "Activating extrinsic calibration tab: stereoframe widget"
-                            )
-                            self.session.set_mode(SessionMode.ExtrinsicCalibration)
-
+                logger.info(f"Activating Charuco Widget")
+                self.session.set_mode(SessionMode.Charuco)
             case 1:
+                logger.info(f"Activating Camera Setup Widget")
+                self.session.set_mode(SessionMode.IntrinsicCalibration)
+            case 2:
+                logger.info(f"Activating Capture Volume Widget")
+                self.capture_volume_widget
+                self.session.set_mode(SessionMode.ExtrinsicCalibration)
+            case 4:
                 logger.info(f"Activate Recording Mode")
                 self.session.set_mode(SessionMode.Recording)
-            case 2:
+            case 5:
                 logger.info(f"Activate Processing Mode")
                 self.session.set_mode(SessionMode.PostProcessing)
                 # may have acquired new recordings
                 self.processing_widget.update_recording_folders()
 
-    def close_current_session(self):
-        pass
 
     def launch_session(self, path_to_folder: str):
         session_path = Path(path_to_folder)
         self.config = Configurator(session_path)
         logger.info(f"Launching session with config file stored in {session_path}")
         self.session = Session(self.config)
-        logger.info("Setting calibration Widget")
-        self.session.stream_tools_loaded_signal.connect(self.load_recording_widget)
 
-        # self.calibration_widget = CalibrationWidget(self.session)
         # can always load charuco
-        
+
         self.charuco_widget = CharucoWidget(self.session)
 
-        # launches without cameras connected, so just throw in a placeholder
+        # launches without cameras connected, so just throw in placeholders
         self.camera_widget = QWidget()
-
         self.recording_widget = QWidget()
+
         if self.session.post_processing_eligible():
             self.processing_widget = PostProcessingWidget(self.session)
         else:
@@ -156,8 +144,8 @@ class MainWindow(QMainWindow):
         self.tab_widget.addTab(self.charuco_widget, "Charuco")
         self.tab_widget.addTab(self.camera_widget, "Cameras")
         self.tab_widget.addTab(self.capture_volume_widget, "CaptureVolume")
-        self.tab_widget.addTab(self.recording_widget, "Rec&ording")
-        self.tab_widget.addTab(self.processing_widget, "&Processing")
+        self.tab_widget.addTab(self.recording_widget, "Recording")
+        self.tab_widget.addTab(self.processing_widget, "Processing")
 
         # default no cameras...can't record
         self.tab_widget.setTabEnabled(1, False)
@@ -167,10 +155,6 @@ class MainWindow(QMainWindow):
             
         old_index = self.tab_widget.currentIndex()
 
-        # self.load_calibration_widget()
-        # self.load_post_processing_widget()
-        # cannot load recording widget until cameras are connected...
-        # self.load_recording_widget()
         self.tab_widget.setCurrentIndex(old_index)
         self.connect_signals()
 
@@ -181,24 +165,13 @@ class MainWindow(QMainWindow):
         """
         self.tab_widget.currentChanged.connect(self.on_tab_changed)
         self.session.unlock_postprocessing.connect(self.load_post_processing_widget)
-        
-
-    # def load_calibration_widget(self):
-    #     calibration_index = self.tab_widget.indexOf(self.calibration_widget)
-    #     self.tab_widget.removeTab(calibration_index)
-    #     self.calibration_widget.deleteLater()
-    #     new_calibration_widget = CalibrationWidget(self.session)
-    #     self.tab_widget.insertTab(
-    #         calibration_index, new_calibration_widget, "&Calibration"
-    #     )
-    #     self.calibration_widget = new_calibration_widget
 
     def load_recording_widget(self):
         recording_index = self.tab_widget.indexOf(self.recording_widget)
         self.tab_widget.removeTab(recording_index)
         self.recording_widget.deleteLater()
         new_recording_widget = RecordingWidget(self.session)
-        self.tab_widget.insertTab(recording_index, new_recording_widget, "Rec&ording")
+        self.tab_widget.insertTab(recording_index, new_recording_widget, "Recording")
         self.recording_widget = new_recording_widget
 
     def load_post_processing_widget(self):
@@ -207,7 +180,7 @@ class MainWindow(QMainWindow):
         self.processing_widget.deleteLater()
         new_processing_widget = PostProcessingWidget(self.session)
         self.tab_widget.insertTab(
-            processing_index, new_processing_widget, "&Processing"
+            processing_index, new_processing_widget, "Processing"
         )
         self.processing_widget = new_processing_widget
 

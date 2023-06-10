cc9ebf66f393b8a36b1c14fc3a4a4a187e0f2f94
mprib <prible@gmail.com>
2023-06-10 13:01:06 -0500

    set_mode prior to launching widget

--- dev/demo/demo_gui_intrinsic_calibration.py
+++ dev/demo/demo_gui_intrinsic_calibration.py
@@ -1,12 +1,15 @@
+
+import pyxy3d.logger
+logger = pyxy3d.logger.get(__name__)
+
 from PyQt6.QtWidgets import QApplication
-from pyxy3d.gui.calibration_widget import CalibrationWidget
 from pyxy3d.gui.camera_config.intrinsic_calibration_widget import IntrinsicCalibrationWidget
 import sys
 from time import sleep
 from pyxy3d import __root__
 from pathlib import Path
 from pyxy3d.configurator import Configurator
-from pyxy3d.session.session import Session
+from pyxy3d.session.session import Session, SessionMode
 import toml
 from pyxy3d import __app_dir__
 
@@ -15,10 +18,11 @@ recent_projects:list = app_settings["recent_projects"]
 
 recent_project_count = len(recent_projects)
 session_path = Path(recent_projects[recent_project_count-1])
+logger.info(f"Attempting to launch session stored at {session_path}")
 
 config = Configurator(session_path)
 session = Session(config)
-   
+session.set_mode(SessionMode.IntrinsicCalibration) 
 app = QApplication(sys.argv)
 window = IntrinsicCalibrationWidget(session)
 

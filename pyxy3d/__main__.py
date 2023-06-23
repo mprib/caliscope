d29aeb7b36456d85c072b81f33f8312d0282b78c
mprib <31831778+mprib@users.noreply.github.com>
2023-06-23 11:14:09 -0500

    clear out deprecated widget option

--- pyxy3d/__main__.py
+++ pyxy3d/__main__.py
@@ -7,24 +7,19 @@ import os
 from PyQt6.QtWidgets import QApplication
 from pathlib import Path
 
-from pyxy3d.gui.calibration_widget import launch_extrinsic_calibration_widget
 from pyxy3d.gui.recording_widget import launch_recording_widget
 from pyxy3d.gui.main_widget import launch_main
 
 def CLI_parser():
     if len(sys.argv) == 1:
-        # logger.warn("No argument supplied")
-        # logger.warn(
-        #     "Please provide specific function to perform: calibrate, record, process"
-        # )
         launch_main()
 
     if len(sys.argv) == 2:
         session_path = Path(os.getcwd())
         launch_widget = sys.argv[1]
 
-        if launch_widget in ["calibrate", "cal", "-c"]:
-            launch_extrinsic_calibration_widget(session_path)
+        # if launch_widget in ["calibrate", "cal", "-c"]:
+        #     launch_extrinsic_calibration_widget(session_path)
 
         if launch_widget in ["record", "rec", "-r"]:
             launch_recording_widget(session_path)

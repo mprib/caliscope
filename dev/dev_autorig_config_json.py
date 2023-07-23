e6f494907488c0a5d97f2f9d22716202097bb1b3
mprib <prible@gmail.com>
2023-07-23 16:56:52 -0500

    bilateral measures being calculated

--- dev/dev_autorig_config_json.py
+++ dev/dev_autorig_config_json.py
@@ -36,14 +36,15 @@ def calculate_distance(xyz_trajectory_data:pd.DataFrame, point1:str, point2:str)
 
     # calculate the average length
     average_length = filtered_distances.mean()
-    print(f"Average Length: {average_length}")
-
+    # print(f"Average Length: {average_length}")
     # histogram of the distances
-    plt.hist(filtered_distances, bins=30, alpha=0.8)
-    plt.xlabel('Distance')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of Distances')
-    plt.show()
+    # plt.hist(filtered_distances, bins=30, alpha=0.8)
+    # plt.xlabel('Distance')
+    # plt.ylabel('Frequency')
+    # plt.title('Histogram of Distances')
+    # plt.show()
+    return average_length
+
 
 symmetrical_measures = {
     "Shoulder_Width":["left_shoulder", "right_shoulder"],
@@ -57,29 +58,29 @@ averaged_measures = {
 }
 
 bilateral_measures = {
-    "Palm": ["index_MCP", "pinky_MCP"],
+    "Palm": ["index_finger_MCP", "pinky_MCP"],
     "Foot":["heel", "foot_index"],  
     "Upper_Arm":["shoulder","elbow"],
     "Forearm":["elbow", "wrist"],
     "Wrist_to_MCP1":["wrist", "thumb_MCP"],
-    "Wrist_to_MCP2":["wrist", "index_MCP"],
-    "Wrist_to_MCP3":["wrist", "middle_MCP"],
-    "Wrist_to_MCP4":["wrist", "ring_MCP"],
+    "Wrist_to_MCP2":["wrist", "index_finger_MCP"],
+    "Wrist_to_MCP3":["wrist", "middle_finger_MCP"],
+    "Wrist_to_MCP4":["wrist", "ring_finger_MCP"],
     "Wrist_to_MCP5":["wrist", "pinky_MCP"],
     "Prox_Phalanx_1":["thumb_MCP", "thumb_IP"],
     "Prox_Phalanx_2":["index_finger_MCP", "index_finger_PIP"],
     "Prox_Phalanx_3":["middle_finger_MCP", "middle_finger_PIP"],
     "Prox_Phalanx_4":["ring_finger_MCP", "ring_finger_PIP"],
-    "Prox_Phalanx_5":["pinky_finger_MCP", "pinky_finger_PIP"],
+    "Prox_Phalanx_5":["pinky_MCP", "pinky_PIP"],
     "Mid_Phalanx_2":["index_finger_PIP", "index_finger_DIP"],
     "Mid_Phalanx_3":["middle_finger_PIP","middle_finger_DIP"],
     "Mid_Phalanx_4":["ring_finger_PIP", "ring_finger_DIP"],
-    "Mid_Phalanx_5":["pinky_finger_PIP", "pinky_finger_DIP"],
-    "Dist_Phalanx_1":["thumb_IP", "thumb_TIP"],
-    "Dist_Phalanx_2":["index_finger_DIP", "index_finger_TIP"],
-    "Dist_Phalanx_3":["middle_finger_DIP","middle_finger_TIP"],
-    "Dist_Phalanx_4":["ring_finger_DIP", "middle_finger_TIP"],
-    "Dist_Phalanx_5":["pinky_finger_DIP", "pinky_finger_TIP"],
+    "Mid_Phalanx_5":["pinky_PIP", "pinky_DIP"],
+    "Dist_Phalanx_1":["thumb_IP", "thumb_tip"],
+    "Dist_Phalanx_2":["index_finger_DIP", "index_finger_tip"],
+    "Dist_Phalanx_3":["middle_finger_DIP","middle_finger_tip"],
+    "Dist_Phalanx_4":["ring_finger_DIP", "middle_finger_tip"],
+    "Dist_Phalanx_5":["pinky_DIP", "pinky_tip"],
     "Thigh_Length":["hip","knee"],
     "Shin_Length": ["knee", "ankle"]
 }
@@ -89,5 +90,16 @@ if __name__ == "__main__":
     xyz_csv_path = Path(__root__,"tests", "reference", "auto_rig_config_data", "xyz_HOLISTIC_OPENSIM_labelled.csv")
 
     xyz_trajectories = pd.read_csv(xyz_csv_path)
-    calculate_distance(xyz_trajectories, 'chin_tip', 'left_ear')
 
+    for measure, points in bilateral_measures.items():
+        logger.info(f"Calculating mean distance (IQR) for {measure}")
+
+        for side in ["left", "right"]:
+            
+            point1 = f"{side}_{points[0]}"
+            point2 = f"{side}_{points[1]}"
+
+            distance = calculate_distance(xyz_trajectories, point1,point2)
+            logger.info(f"Between {point1} and {point2} the mean distance is {distance}")     
+        
+            

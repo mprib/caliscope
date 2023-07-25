f8ba39450b5ab3e7e967345e854ee86e87bbff25
mprib <31831778+mprib@users.noreply.github.com>
2023-07-25 08:21:10 -0500

    loading point estimates to dict

--- dev/dev_revised_point_estimate_storage.py
+++ dev/dev_revised_point_estimate_storage.py
@@ -1,6 +1,5 @@
-#%%
 
-
+# %%
 import pyxy3d.logger
 logger = pyxy3d.logger.get(__name__)
 
@@ -16,29 +15,21 @@ test_session_path = Path(__root__, "tests", "sessions","post_optimization")
 config = Configurator(test_session_path)
 
 point_estimates = config.get_point_estimates()
-point_estimates_dict = point_estimates.to_flat_dict()
-#%%
+point_estimates_dict = asdict(point_estimates)
 
-
-
-flat_dict = {}
 for key, params in point_estimates_dict.items():
-    match key:
-        case "img":
-            xy = params
-            flat_dict["img_x"] = xy[:,0].tolist()
-            flat_dict["img_y"] = xy[:,1].tolist()
-        case "obj":
-            xyz = params
-            flat_dict["obj_x"] = xyz[:,0].tolist()
-            flat_dict["obj_y"] = xyz[:,1].tolist()
-            flat_dict["obj_z"] = xyz[:,2].tolist()
-        case _:
-            flat_dict[key] = params.tolist() 
+    point_estimates_dict[key] = params.tolist()
 
 
-for key, value in flat_dict.items():
-    print(key, len(value))
+def shape(lst):
+    try:
+        return [len(lst)] + shape(lst[0])
+    except TypeError:
+        return []
+ 
+
+for key, value in point_estimates_dict.items():
+    print(key, shape(value))
 # point_estimates_path = Path(test_session_path, "point_estimates.csv")
 
 # pd.DataFrame(flat_dict).to_csv(point_estimates_path)

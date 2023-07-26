da58427c889e40155b362c44aeae709b724a100a
mprib <prible@gmail.com>
2023-07-26 17:17:25 -0500

    still failing

--- pyxy3d/gui/min_rep_failing.py
+++ pyxy3d/gui/min_rep_failing.py
@@ -1,16 +1,12 @@
 from PySide6.QtWidgets import ( QApplication, QWidget, QLabel, QVBoxLayout,)
 from PySide6.QtCore import Signal,Slot, QThread
-import sys
-
-from time import perf_counter, sleep
-
+from time import sleep
 import random
  
 class MyWidget(QWidget):
      
     def __init__(self):
-
-        super(MyWidget, self).__init__()
+        super().__init__()
 
         self.label = QLabel()
 
@@ -20,24 +16,24 @@ class MyWidget(QWidget):
         self.setLayout(layout)
 
         self.emitter = EmitterThread()
-        self.emitter.dict_signal.connect(self.update_label)
+        self.emitter.my_signal.connect(self.update_label)
         self.emitter.start()
         
     @Slot(dict) 
     def update_label(self, data):
         "Unravel dropped fps dictionary to a more readable string"
         print(f"Just received {data}")
-        self.label.setText(f"{data['1']}")
+        self.label.setText(f"{data[1]}")
          
 class EmitterThread(QThread):
-    dict_signal = Signal(dict)
+    my_signal = Signal(dict)
        
     def run(self):
         while True:
 
-            random_dictionary = {1:str(random.randint(1,10))}
-            self.dict_signal.emit(random_dictionary)
-            print(f"Emit dictionary: {random_dictionary}")
+            emitted_dictionary = {1:str(random.randint(1,10))}
+            self.my_signal.emit(emitted_dictionary)
+            print(f"Emit dictionary: {emitted_dictionary}")
 
             sleep(1)
 

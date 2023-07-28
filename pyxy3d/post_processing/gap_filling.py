d5cd71731c5d93867dad181fe63badabb19d6e24
mprib <prible@gmail.com>
2023-07-28 10:26:12 -0500

    spelling

--- pyxy3d/post_processing/gap_filling.py
+++ pyxy3d/post_processing/gap_filling.py
@@ -63,8 +63,8 @@ def xy_gap_fill(xy_base:pd.DataFrame, max_gap_size=3) -> pd.DataFrame:
 
         # Interpolate the values for img_loc_x and img_loc_y, limit the interpolation to GAP_SIZE_TO_FILL
         merged['frame_time'] = merged['frame_time'].interpolate(method='linear', limit=max_gap_size).astype('float64')
-        merged['img_loc_x'] = merged['img_loc_x'].interpolate(method='linear', limit=max_gap_size).round().astype('Int64')
-        merged['img_loc_y'] = merged['img_loc_y'].interpolate(method='linear', limit=max_gap_size).round().astype('Int64')
+        merged['img_loc_x'] = merged['img_loc_x'].interpolate(method='linear', limit=max_gap_size).astype('Int64')
+        merged['img_loc_y'] = merged['img_loc_y'].interpolate(method='linear', limit=max_gap_size).astype('Int64')
 
         # Append to the overall DataFrame
         xy_filled = pd.concat([xy_filled, merged])
@@ -84,38 +84,32 @@ def xyz_gap_fill(xyz_base:pd.DataFrame, max_gap_size=3) -> pd.DataFrame:
         loc_z
     """
     # Initialize a DataFrame to store the data with filled gaps
-    xy_filled = pd.DataFrame()
-
-    if "frame_index" in xyz_base.columns:
-        index_key = "frame_index"
-    else:
-        index_key = "sync_index"
+    xyz_filled = pd.DataFrame()
 
     # Loop through each combination of port and point_id
-    for (port, point_id), group in xyz_base.groupby(['port', 'point_id']):
+    for point_id, group in xyz_base.groupby(['point_id']):
         # Sort by frame_index to ensure the data is in order
-        group = group.sort_values(index_key)
+        group = group.sort_values("sync_index")
     
         # Create a new DataFrame with all frame_index values in the range
-        all_frames = pd.DataFrame({index_key: np.arange(group[index_key].min(), group[index_key].max() + 1)})
-        all_frames['port'] = port
+        all_frames = pd.DataFrame({"sync_index": np.arange(group["sync_index"].min(), group["sync_index"].max() + 1)})
         all_frames['point_id'] = point_id
 
         # Merge the actual data with the all_frames DataFrame
-        merged = pd.merge(all_frames, group, on=['port', 'point_id', index_key], how='left')
+        merged = pd.merge(all_frames, group, on=['point_id', "sync_index"], how='left')
 
         # Calculate the size of each gap
-        merged['gap_size'] = merged['img_loc_x'].isnull().astype(int).groupby((merged['img_loc_x'].notnull()).cumsum()).cumsum()
+        merged['gap_size'] = merged['x_coord'].isnull().astype(int).groupby((merged['x_coord'].notnull()).cumsum()).cumsum()
 
         # Remove the rows where the gap is larger than GAP_SIZE_TO_FILL
         merged = merged[merged['gap_size'] <= max_gap_size]
 
         # Interpolate the values for img_loc_x and img_loc_y, limit the interpolation to GAP_SIZE_TO_FILL
-        merged['frame_time'] = merged['frame_time'].interpolate(method='linear', limit=max_gap_size).astype('float64')
-        merged['img_loc_x'] = merged['img_loc_x'].interpolate(method='linear', limit=max_gap_size).round().astype('Int64')
-        merged['img_loc_y'] = merged['img_loc_y'].interpolate(method='linear', limit=max_gap_size).round().astype('Int64')
+        merged['x_coord'] = merged['x_coord'].interpolate(method='linear', limit=max_gap_size).astype('float64')
+        merged['y_coord'] = merged['y_coord'].interpolate(method='linear', limit=max_gap_size).astype('float64')
+        merged['z_coord'] = merged['z_coord'].interpolate(method='linear', limit=max_gap_size).astype('float64')
 
         # Append to the overall DataFrame
-        xy_filled = pd.concat([xy_filled, merged])
+        xyz_filled = pd.concat([xyz_filled, merged])
 
-    return xy_filled
+    return xyz_filled

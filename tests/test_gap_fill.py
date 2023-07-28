e9e36ce3e1912e21ab0c8e5ac51e5b4de2ad9ee2
mprib <prible@gmail.com>
2023-07-28 11:30:48 -0500

    appears to be working...

--- tests/test_gap_fill.py
+++ tests/test_gap_fill.py
@@ -9,7 +9,7 @@ import numpy as np
 
 from pathlib import Path
 from pyxy3d.trackers.tracker_enum import TrackerEnum
-from pyxy3d.post_processing.gap_filling import xy_gap_fill
+from pyxy3d.post_processing.gap_filling import xy_gap_fill, xyz_gap_fill
 from pyxy3d import __root__
 
 def test_gap_fill_xy():
@@ -24,14 +24,35 @@ def test_gap_fill_xy():
     xy_all_filled = xy_gap_fill(xy_all_base)
     filled_length = xy_all_filled.shape[0]
 
+    if __name__ == "__main__":
+        xy_filled_path = Path(xy_all_base_path.parent, f"xy_{tracker_enum.name}_filled.csv")
+        xy_all_filled.to_csv(xy_filled_path, index=False)
+
     # a coupLe basic assertions
     assert base_length < filled_length
     assert xy_all_filled["gap_size"].max() > 0
 
+def test_gap_fill_xyz():
+    recording_directory = Path(__root__, "tests", "reference", "base_data")
+    tracker_enum = TrackerEnum.HOLISTIC_OPENSIM
+
+    # Load the data
+    xyz_all_base_path = Path(recording_directory,tracker_enum.name, f"xyz_{tracker_enum.name}.csv")
+    logger.info(f"Reading in raw xy data located at {xyz_all_base_path}")
+    xyz_all_base = pd.read_csv(xyz_all_base_path)
+    base_length = xyz_all_base.shape[0]
+    xyz_all_filled = xyz_gap_fill(xyz_all_base)
+    filled_length = xyz_all_filled.shape[0]
+
+    if __name__ == "__main__":
+        xyz_filled_path = Path(xyz_all_base_path.parent, f"xyz_{tracker_enum.name}_filled.csv")
+        xyz_all_filled.to_csv(xyz_filled_path, index=False)
+
+    # a coupLe basic assertions
+    assert base_length < filled_length
+    assert xyz_all_filled["gap_size"].max() > 0
 if __name__ == "__main__":
-    test_gap_fill_xy()
+    test_gap_fill_xyz()
 
 # Write the DataFrame to a new CSV file
-# xy_filled_path = Path(xy_all_base_path.parent, f"xy_{tracker_enum.name}_filled.csv")
-# xy_all_filled.to_csv(xy_filled_path, index=False)
 # %%

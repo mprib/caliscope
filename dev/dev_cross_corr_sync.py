686c1335e9e6db6cc5e49d97bd4f2642b6d17941
mprib <prible@gmail.com>
2023-08-02 18:34:22 -0500

    successfully creating reduced frames for testing

--- dev/dev_cross_corr_sync.py
+++ dev/dev_cross_corr_sync.py
@@ -4,6 +4,7 @@ from threading import Thread
 
 logger = get(__name__)
 import pandas as pd
+import numpy as np
 from pathlib import Path
 from moviepy.editor import VideoFileClip
 from pyxy3d.trackers.tracker_enum import TrackerEnum
@@ -69,34 +70,84 @@ def save_point_data(mp4_path:Path, tracker:Tracker, camera_index:int, rotation_c
     pd.DataFrame(point_data).to_csv(point_data_path)
     
 
-recording_directory = Path(
-    r"C:\Users\Mac Prible\OneDrive\pyxy3d\test_record\recording_1"
-)
+ 
+ 
+def create_points_in_directory(recording_directory:Path, tracker:Tracker):
 
-mp4s = recording_directory.glob("*.mp4")
+    mp4s = recording_directory.glob("*.mp4")
 
-charuco = Charuco(
-    columns=4,
-    rows=5,
-    board_height=11,
-    board_width=8.5,
-    dictionary="DICT_4X4_50",
-    units="inch",
-    aruco_scale=0.75,
-    square_size_overide_cm=5.4,
-    inverted=True,
-)
+    video_streams = []
 
-tracker = CharucoTracker(charuco)
-video_streams = []
+    camera_index = 0
+    rotation_count = 0  # none of the files are rotated...
+    # resolutions = get_resolutions_of_all_mp4s_in_folder(recording_directory)
 
-camera_index = 0
-rotation_count = 0  # none of the files are rotated...
-# resolutions = get_resolutions_of_all_mp4s_in_folder(recording_directory)
+    for mp4_path in mp4s:
+        logger.info(f"Begin processing of {mp4_path.name}")
 
-for mp4_path in mp4s:
-    logger.info(f"Begin processing of {mp4_path.name}")
+        thread = Thread(target=save_point_data, args = [mp4_path, tracker, camera_index, rotation_count])
+        thread.start()
+        camera_index += 1
 
-    thread = Thread(target=save_point_data, args = [mp4_path, tracker, camera_index, rotation_count])
-    thread.start()
-    camera_index += 1
+
+def remove_random_frames(file_path, fps=30, seed=42):
+    # Set seed for reproducibility
+    np.random.seed(seed)
+    
+    # Load data
+    data = pd.read_csv(file_path)
+    
+    # Calculate the number of frames equivalent to 2 seconds
+    max_frames_to_remove = 2 * fps
+    
+    # Generate random number of frames to remove from the beginning and end
+    frames_to_remove_start = np.random.randint(0, max_frames_to_remove)
+    frames_to_remove_end = np.random.randint(0, max_frames_to_remove)
+    
+    min_frame = data["sync_index"].min()
+    max_frame = data["sync_index"].max()
+    
+    
+    # Remove frames
+    data = data.query(f"sync_index > {min_frame + frames_to_remove_start} and sync_index < {max_frame-frames_to_remove_end}")
+    
+    # Reset index
+    data.reset_index(drop=True, inplace=True)
+    
+    # Generate new file path
+    file_path = Path(file_path)
+    new_file_path = file_path.with_name(file_path.stem + "_alt" + file_path.suffix)
+    
+    # Save to new file
+    data.to_csv(new_file_path, index=False)
+    
+
+if __name__ == "__main__":
+    recording_directory = Path(
+        r"C:\Users\Mac Prible\OneDrive\pyxy3d\test_record\recording_1"
+    )
+
+    charuco = Charuco(
+        columns=4,
+        rows=5,
+        board_height=11,
+        board_width=8.5,
+        dictionary="DICT_4X4_50",
+        units="inch",
+        aruco_scale=0.75,
+        square_size_overide_cm=5.4,
+        inverted=True,
+    )
+
+    tracker = CharucoTracker(charuco)
+
+    # comment this out so you don't have to rerun it every time    
+    # create_points_in_directory(recording_directory, tracker)
+
+    for csv_path in recording_directory.glob("*_alt.csv"):
+        logger.info(f"removing file contained at {csv_path}")
+        csv_path.unlink()
+
+    for csv_path in recording_directory.glob("point_data_*"):
+        logger.info(f"Creating alternate data for test purposes with beginning and ending data deleted")
+        remove_random_frames(csv_path)

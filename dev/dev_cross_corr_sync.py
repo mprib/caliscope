e4f39ac046e8f1c36e7415db9cdad7982fa9bc0b
mprib <prible@gmail.com>
2023-08-04 13:09:29 -0500

    building out prelim data pipeline and EDA viz

--- dev/dev_cross_corr_sync.py
+++ dev/dev_cross_corr_sync.py
@@ -1,6 +1,6 @@
 
 #%%
-import matplotlib.pyplot as plt
+from plotnine import *
 from pyxy3d.logger import get
 import cv2
 from threading import Thread
@@ -15,24 +15,44 @@ recording_directory = Path(
     r"C:\Users\Mac Prible\OneDrive\pyxy3d\test_record\recording_1"
 )
 
-
-# consolidate all data created by dev_prep_sync_test_data
-csv_data = []
-for csv_path in recording_directory.glob("*_alt.csv"):
-    logger.info(f"Aggregating data from {csv_path}")
-    csv_data.append(pl.read_csv(csv_path))
-
 data = pl.read_csv(Path(recording_directory,"combined_gap_filled_alt.csv"))
-# for csv_path in recording_directory.glob("*_alt.csv"):
-#     if data is None:
-#         data = pd.read_csv(csv_path)
-        
-#     else:
-#         current_data = pd.read_csv(csv_path)
-#         data = pd.concat([data,current_data])
 
-# for port in data["port"].unique():
-#     for point_id in data.query(f"port == {port}")["point_id"].unique():
-#         logger.info(f"Normalizing data for port {port} and point_id {point_id}")
+#%%
+c = pl.col
 
-                
+def assign_tracked_group(df:pl.DataFrame):
+    change_mask  = df.with_columns((c("frame_delta")!=c("frame_delta").shift()).alias("change_mask"))
+    return change_mask.with_columns(c("change_mask").cumsum().alias("tracked_subgroup"))
+
+def normalize_y_displacement(df: pl.DataFrame) -> pl.DataFrame:
+    mean_y = df['img_loc_y'].mean()
+    std_y = df['img_loc_y'].std()
+    df = df.with_columns(((c('img_loc_y') - mean_y) / std_y).alias("norm_y"))
+    return df
+
+# Applying the function in the pipeline
+df_sync = (data
+            .sort(["port", "point_id", "frame_index"])
+            .with_columns((c("frame_index") - c("frame_index").shift()).alias("frame_delta"))
+            .groupby(["port", "point_id"]).apply(assign_tracked_group)
+            .with_columns(pl.concat_str([c("port"), c("point_id"), c("tracked_subgroup")], separator="_").alias("track_id"))
+            .groupby("track_id").apply(normalize_y_displacement)
+           )
+
+df_sync
+
+#%%
+
+df_plt =(df_sync
+        #  .filter(c("port").is_in([0,1]))
+         .filter(c("point_id")==7)
+        #  .to_pandas()
+        )
+
+plot = (ggplot(df_plt)+
+        aes(x="frame_time", y = "norm_y", color = "track_id")+
+        facet_grid("port ~.")+
+        geom_point()
+)
+plot
+# %%

a072bc11752d2eb986fa46c2a2510848683ab16d
mprib <prible@gmail.com>
2023-11-04 14:10:06 -0500

    beginning to build out a test, but not complete

--- tests/test_intrinsic_calibrator.py
+++ tests/test_intrinsic_calibrator.py
@@ -1,8 +1,12 @@
-import pyxy3d.logger
 from pathlib import Path
+from queue import Queue
+
 from pyxy3d import __root__
 from pyxy3d.helper import copy_contents
-
+from pyxy3d.calibration.charuco import Charuco
+from pyxy3d.trackers.charuco_tracker import CharucoTracker
+from pyxy3d.recording.recorded_stream import RecordedStream
+import pyxy3d.logger
 
 logger = pyxy3d.logger.get(__name__)
 def test_intrinsic_calibrator():
@@ -13,8 +17,24 @@ def test_intrinsic_calibrator():
     copy_contents(original_data_path,destination_path)
 
 
+    recording_directory = Path(
+        __root__, "tests", "sessions", "post_monocal", "calibration", "extrinsic"
+    )
 
+    charuco = Charuco(
+        4, 5, 11, 8.5, aruco_scale=0.75, square_size_overide_cm=5.25, inverted=True
+    )
 
+    charuco_tracker = CharucoTracker(charuco)
+    
+    stream  = RecordedStream(recording_directory,port=1,rotation_count=0, tracker=charuco_tracker)
+
+    frame_q = Queue()
+    stream.subscribe(frame_q)
+    
+    stream.play_video()
+    
+    
 if __name__ == "__main__":
     test_intrinsic_calibrator()
     

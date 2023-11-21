da98ad58efef5ac112b34a12de7526ae8384a89a
mprib <prible@gmail.com>
2023-11-21 10:43:16 -0600

    Main window --> Widget

--- pyxy3d/gui/prerecorded_intrinsic_calibration/multiplayback_widget.py
+++ pyxy3d/gui/prerecorded_intrinsic_calibration/multiplayback_widget.py
@@ -1,10 +1,18 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QTabWidget, QWidget
+from PySide6.QtWidgets import (
+    QApplication,
+    QVBoxLayout,
+    QTabWidget,
+    QWidget,
+)
 from pathlib import Path
-from pyxy3d.gui.prerecorded_intrinsic_calibration.playback_widget import IntrinsicCalibrationWidget
+from pyxy3d.gui.prerecorded_intrinsic_calibration.playback_widget import (
+    IntrinsicCalibrationWidget,
+)
 from pyxy3d.controller import Controller
 
-class MainWindow(QMainWindow):
+
+class MultiPlayback(QWidget):
     def __init__(self, controller):
         super().__init__()
         self.controller = controller
@@ -13,20 +21,17 @@ class MainWindow(QMainWindow):
     def initUI(self):
         self.tabWidget = QTabWidget(self)
         self.tabWidget.setTabPosition(QTabWidget.West)  # Tabs on the left side
-
         self.loadTabs()
 
-        self.setCentralWidget(self.tabWidget)
-        self.setWindowTitle("Intrinsic Calibration Tabs")
-        self.resize(1200, 800)
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self.tabWidget)
+        # self.resize(1200, 800)
 
     def loadTabs(self):
-        # Assuming controller.load_intrinsic_streams() loads streams and 
-        # controller.streams is an iterable of stream IDs
-
         for port in self.controller.intrinsic_streams:
             tab = IntrinsicCalibrationWidget(controller=self.controller, port=port)
-            self.tabWidget.addTab(tab, f"Stream {port}")
+            self.tabWidget.addTab(tab, f"Cam {port}")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -36,6 +41,6 @@ if __name__ == "__main__":
     controller.add_all_cameras_in_intrinsics_folder()
     controller.load_intrinsic_streams()
 
-    mainWin = MainWindow(controller)
+    mainWin = MultiPlayback(controller)
     mainWin.show()
     sys.exit(app.exec())

8cd8b75349f6f730ed5c9fe9638797a41a0e7903
mprib <prible@gmail.com>
2024-01-22 09:46:24 -0600

    building out gui demo

--- dev/demo/demo_gui_triangulation_visualizer.py
+++ dev/demo/demo_gui_triangulation_visualizer.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 from pyxy3d.controller import Controller
 from pyxy3d.gui.vizualize.playback_triangulation_widget import PlaybackTriangulationWidget
+from pyxy3d.trackers.holistic_tracker import HolisticTracker
 
 app = QApplication(sys.argv)
 workspace_dir = Path(r"C:\Users\Mac Prible\OneDrive - The University of Texas at Austin\research\pyxy3d\example_project")
@@ -10,9 +11,10 @@ workspace_dir = Path(r"C:\Users\Mac Prible\OneDrive - The University of Texas at
 controller = Controller(workspace_dir)
 controller.load_camera_array()
 camera_array = controller.camera_array
+tracker = HolisticTracker()
 
 xyz_history = Path(workspace_dir, "recordings","test_recording", "HOLISTIC", "xyz_HOLISTIC.csv")
-window = PlaybackTriangulationWidget(camera_array=camera_array,xyz_history_path=xyz_history)
+window = PlaybackTriangulationWidget(camera_array=camera_array,xyz_history_path=xyz_history, )
 
 window.show()
 

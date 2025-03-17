e09260deb71b13ee62652508027f9f55236fd1d8
mprib <prible@gmail.com>
2025-03-17 13:27:01 -0500

    new demo data

--- dev/demo/demo_gui_capture_volume.py
+++ dev/demo/demo_gui_capture_volume.py
@@ -5,9 +5,11 @@ from caliscope.controller import Controller
 from caliscope.gui.vizualize.calibration.capture_volume_widget import CaptureVolumeWidget
 
 app = QApplication(sys.argv)
-workspace_dir = Path(r"C:\Users\Mac Prible\OneDrive\caliscope\4_cam_prerecorded_practice_working")
+workspace_dir = Path(r"C:\Users\Mac Prible\repos\caliscope\dev\key_error_11")
+                     
+
 controller = Controller(workspace_dir)
-# controller.load_camera_array()
+controller.load_camera_array()
 controller.load_estimated_capture_volume()
 window = CaptureVolumeWidget(controller)
 

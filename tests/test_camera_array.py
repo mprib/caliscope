4b259fdeb3026aa4cc63a10a43d930c1a23fc2a6
Mac Prible <prible@gmail.com>
2025-08-26 17:50:48 -0500

    adjust so now shouldn't have cams 4 or 5 (for different reasons)

--- tests/test_camera_array.py
+++ tests/test_camera_array.py
@@ -15,38 +15,6 @@ from caliscope.helper import copy_contents
 logger = caliscope.logger.get(__name__)
 
 
-def test_missing_extrinsics_old():
-    version = "not_sufficient_stereopairs"
-    original_session_path = Path(__root__, "tests", "sessions", version)
-    session_path = Path(
-        original_session_path.parent.parent,
-        "sessions_copy_delete",
-        version,
-    )
-    copy_contents(original_session_path, session_path)
-
-    config = Configurator(session_path)
-    xy_data_path = Path(session_path, "xy_CHARUCO.csv")
-    config.get_camera_array()
-    config.get_charuco()
-
-    logger.info("Creating stereocalibrator")
-    stereocalibrator = StereoCalibrator(config.config_toml_path, xy_data_path)
-    logger.info("Initiating stereocalibration")
-    stereocalibrator.stereo_calibrate_all(boards_sampled=10)
-
-    logger.info("stereocalibration complete")
-    logger.info("Initializing estimated camera positions based on best daisy-chained stereopairs")
-    camera_array: CameraArray = CameraArrayInitializer(config.config_toml_path).get_best_camera_array()
-    logger.info("Camera Poses estimated from stereocalibration")
-    # should have posed all ports but 5
-    assert list(camera_array.posed_cameras.keys()) == [1, 2, 3, 4, 6]
-    assert list(camera_array.unposed_cameras.keys()) == [5]
-
-    # when creating extrinsic parameters shouldn't have camera 5.
-    # should be able to extract params from complete extrinsics vector and map to individual cam params
-
-
 def test_missing_extrinsics():
     version = "not_sufficient_stereopairs"
     original_session_path = Path(__root__, "tests", "sessions", version)
@@ -59,8 +27,6 @@ def test_missing_extrinsics():
 
     config = Configurator(session_path)
     xy_data_path = Path(session_path, "xy_CHARUCO.csv")
-    config.get_camera_array()
-    config.get_charuco()
 
     logger.info("Creating stereocalibrator")
     stereocalibrator = StereoCalibrator(config.config_toml_path, xy_data_path)
@@ -72,18 +38,19 @@ def test_missing_extrinsics():
     camera_array: CameraArray = CameraArrayInitializer(config.config_toml_path).get_best_camera_array()
     logger.info("Camera Poses estimated from stereocalibration")
 
-    # should have posed all ports but 5
+    # should have posed all ports but 4 and 5
     # Using set for posed_cameras to avoid order dependency
-    assert set(camera_array.posed_cameras.keys()) == {1, 2, 3, 4, 6}
-    assert list(camera_array.unposed_cameras.keys()) == [5]
+    assert set(camera_array.posed_cameras.keys()) == {1, 2, 3, 6}
+    assert list(camera_array.unposed_cameras.keys()) == [4, 5]
 
     # when creating extrinsic parameters shouldn't have camera 5.
     extrinsic_params = camera_array.get_extrinsic_params()
     assert extrinsic_params is not None, "Extrinsic parameters should not be None"
-    assert extrinsic_params.shape == (5, 6), "Shape should be (5 posed cameras, 6 params)"
+    assert extrinsic_params.shape == (4, 6), "Shape should be (4 posed cameras, 6 params)"
 
     # camera 5 should not be in the index used for optimization parameter mapping
     assert 5 not in camera_array.posed_port_to_index
+    assert 4 not in camera_array.posed_port_to_index
 
     # Verify that the order of cameras in the extrinsic_params array is correct
     logger.info("Verifying order of extrinsic parameters vector...")
@@ -116,4 +83,5 @@ def test_missing_extrinsics():
 
 if __name__ == "__main__":
     test_missing_extrinsics()
+    print("end")
     # test_deterministic_consistency()

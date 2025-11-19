c224fa8ffa27bf8c255bb6b3dfa57043df15f93c
Mac Prible <prible@gmail.com>
2025-11-19 12:34:01 -0600

    cleaner demo of sync frames

--- scripts/demo/demo_synched_frames_widget.py
+++ scripts/demo/demo_synched_frames_widget.py
@@ -1,37 +1,38 @@
+import logging
 from PySide6.QtWidgets import QApplication
 import sys
 
 from caliscope.gui.synched_frames_display import SyncedFramesDisplay
 from caliscope.controller import Controller
+from caliscope.synchronized_stream_manager import SynchronizedStreamManager
 
 from pathlib import Path
 
-from caliscope import __root__
-from caliscope.helper import copy_contents
-import caliscope.logger
 from time import sleep
+from caliscope.trackers.holistic.holistic_tracker import HolisticTracker
 
-logger = caliscope.logger.get(__name__)
+logger = logging.getLogger(__name__)
+
 app = QApplication(sys.argv)
-# Define the input file path here.
-original_workspace_dir = Path(__root__, "tests", "sessions", "mediapipe_calibration")
-workspace_dir = Path(__root__, "tests", "sessions_copy_delete", "4_cam_record")
 
-copy_contents(original_workspace_dir, workspace_dir)
+demo_workspace = Path("/home/mprib/caliscope_projects/minimal_project")
+# video_dir = demo_workspace / "calibration/extrinsic"
+video_dir = demo_workspace / "recordings/sitting"
+controller = Controller(demo_workspace)
+camera_array = controller.config.get_camera_array()
+# charuco = controller.config.get_charuco()
+# tracker = CharucoTracker(charuco)
+tracker = HolisticTracker()
+sync_stream_manager = SynchronizedStreamManager(video_dir, camera_array.cameras, tracker=tracker)
 
-controller = Controller(workspace_dir)
-controller.load_camera_array()
-controller.load_extrinsic_stream_manager()
+sync_stream_manager.process_streams(fps_target=10)
+window = SyncedFramesDisplay(sync_stream_manager)
 
-controller.extrinsic_stream_manager.process_streams(fps_target=100)
-window = SyncedFramesDisplay(controller.extrinsic_stream_manager)
 # need to let synchronizer spin up before able to display frames
-# need to let synchronizer spin up before able to display frames
-
-while not hasattr(controller.extrinsic_stream_manager.synchronizer, "current_sync_packet"):
+while not hasattr(sync_stream_manager.synchronizer, "current_sync_packet"):
     sleep(0.25)
 
 window.show()
-window.resize(800, 600)
+window.resize(1050, 1050)
 logger.info("About to show window")
 sys.exit(app.exec())
